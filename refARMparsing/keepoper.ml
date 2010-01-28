4ede9ea64aff1d99fe5a1830935b4951b8f3dc18
Jean-François Monin <monin@users.gforge.inria.noreply>
2010-01-28 04:46:45 +0000

    modes d'adressage dans refARMparsing
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@164 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- refARMparsing/keepoper.ml
+++ refARMparsing/keepoper.ml
@@ -33,7 +33,7 @@ let ident =
   let ident c s = Buffer.clear bu; Buffer.add_char bu c; ident_aux s in
   ident
 
-(* Full line *)
+(* Returns full line as string *)
 let take_eol =
   let bu = Buffer.create 80 in 
   let rec aux = parser
@@ -49,7 +49,7 @@ let rec horner n = parser
   | [< '  '0'..'9' as c; s >] -> horner (10 * n + valdigit c) s
   | [< >] -> n
 
-
+(* Jumps to eol *)
 let rec eat_eol = parser 
   | [< ''\n'; s >] -> ()
   | [< 'c; s >] -> eat_eol s
@@ -69,8 +69,12 @@ let rec title = parser
   | [< ''A'..'Z' | 'a'..'z' as c; s = take_eol c >] -> s
   | [< >] -> raise Not_header
 
-
+(* ex. A4.1.2 ADC  *)
 type header = Header of char * int * int list * string
+
+(* In case of failure we want the contents of the line *)
+(* SH = Some Header, NH = No Header *)
+type opt_header = SH of header | NH of string
 
 let print_header = function
   | Header (c, n, l, s) -> 
@@ -84,33 +88,43 @@ let print_header = function
 let end_header c = parser
   | [< n, l = seqint1; t = title >] -> Header (c, n, l, t)
 
-let optheader = parser
+let rec blanks_alpha = parser
+  | [< '' ' ; s >] -> blanks_alpha s
   | [< ''A'..'Z' as c; s >] -> 
-      try Some (end_header c s )
-      with Not_header -> let () = eat_eol s in None
+      try SH (end_header c s )
+      with Not_header -> NH (take_eol c s) (* TO REMOVE  let () = eat_eol s in None*)
 
+
+exception PB_to_next_header
 
 let rec to_next_header = parser
-  | [< ho = optheader; s >] ->
+  | [< ho = blanks_alpha; s >] ->
     (match ho with
-      | None -> to_next_header s
-      | Some _ -> ho)
+      | NH _ -> to_next_header s
+      | SH h -> h)
   | [< () = eat_eol; s >] -> to_next_header s
-  | [< >] -> None
+  | [< >] -> raise PB_to_next_header
 
+(* Goes after the next header h which satisfies f h and returns h *)
 let rec to_given_header f = parser
-  | [< ho = to_next_header; s >] ->
-    (match ho with
-      | None -> raise Not_found
-      | Some h -> if f h then h else to_given_header f s)
-  | [< >] -> raise Not_found
+  | [< h = to_next_header; s >] -> if f h then h else to_given_header f s
 
+(* Texts use of locating the beginning of interesting parts *)
 let alpha = "Alphabetical list of ARM instructions"
 let genotes = "General notes"
+let addrmodes = "Addressing Mode "
+let beforeopdescr = "Shifted register operand value"
+
+(* p is a prefix of s *)
+let is_pref p s = 
+  let l = String.length p in
+  l <= String.length s && String.sub s 0 l = p
 
 let filtitle t0 (Header (c, n, l, t)) =  t = t0
 let filpart c0 (Header (c, n, l, t)) =   c = c0 && List.length l = 2
 let filendinstr (Header (c, n, l, t)) =  List.length l = 1
+
+let preftitle t0 (Header (c, n, l, t)) =  is_pref t0 t
 
 let to_next_Ainstr = to_given_header (filpart 'A')
 
@@ -122,36 +136,72 @@ let rec to_contents_instr = parser
       else to_contents_instr s
   | [< () = eat_eol; s >] -> to_contents_instr s
 
-let rec in_op = parser 
-  | [< ''\n'; s >] -> in_op1 s
+let rec in_operation = parser 
+  | [< ''\n'; s >] -> in_operation1 s
   | [< 'c; a = take_eol c; s >] -> 
-    print_endline a; in_op s
-and in_op1 = parser 
+    print_endline a; in_operation s
+and in_operation1 = parser 
   | [< ''\n' >] -> ()
   | [< 'c; a = take_eol c; s >] ->
-    print_endline a; in_op s
+    print_endline a; in_operation s
 
 (* Only part A is considered in ARM manual *)
+
+type stop_ou_encore = Stop | Continue | Op of header 
+exception PB_check_then_to_operation_or_next_header
+
+let rec to_operation_or_next_header h = parser
+  | [< ba = blanks_alpha; s >] ->
+      (match ba with 
+      | NH "Operation" -> Op h
+      | NH _ -> to_operation_or_next_header h s
+      | SH h1 -> check_then_to_operation_or_next_header h1 s )
+  | [< () = eat_eol; s >] -> to_operation_or_next_header h s
+and check_then_to_operation_or_next_header h s = 
+      if filpart 'A' h then to_operation_or_next_header h s
+      else if filendinstr h then Stop
+      else Continue
+
+
 let rec loop_instrs = parser
-  | [< ho = to_next_header; s >] -> 
-    (match ho with
-      | None -> ()
-      | Some h ->
-	  if filpart 'A' h then 
-	    begin
-	      print_header h;
-	      to_contents_instr s;
-	      in_op s;
-              loop_instrs s
-	    end
-	  else if filendinstr h then ()
-	  else loop_instrs s)
+  | [< h = to_next_header; s >] -> 
+      (match check_then_to_operation_or_next_header h s with
+	 | Op h1 -> 
+	     begin
+	       print_header h1;
+	       in_operation s;
+               loop_instrs s
+	     end
+	 | Stop -> ()
+	 | Continue -> loop_instrs s
+       )
+
+(* 
+let rec loop_instrs = parser
+  | [< h = to_next_header; s >] -> 
+      if filpart 'A' h then 
+	begin
+	  print_header h;
+	  to_contents_instr s;
+	  in_operation s;
+          loop_instrs s
+	end
+      else if filendinstr h then ()
+      else loop_instrs s
   | [< >] -> ()
+*)
 
 let main = parser 
     [< _ = to_given_header (filtitle alpha);
        _ = to_given_header (filtitle genotes);
-       () = loop_instrs >]
+       () = loop_instrs;
+       _ = to_given_header (preftitle addrmodes);
+       () = loop_instrs;
+       () = loop_instrs;
+       () = loop_instrs;
+       () = loop_instrs;
+       () = loop_instrs;
+    >]
   -> ()
 
 let () = main (Stream.of_channel stdin)
