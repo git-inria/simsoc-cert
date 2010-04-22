bcede775c2208c38b0d0fa752e8c575e56222a24
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2010-04-22 04:18:07 +0000

    added access functions
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@237 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- coq/State.v
+++ coq/State.v
@@ -22,17 +22,15 @@ Record state : Type := mk_state {
   scc : SCC.state
 }.
 
+Definition cpsr (s : state) : word := cpsr (proc s).
+
 Definition set_cpsr (s : state) (w : word) : state :=
   mk_state (set_cpsr (proc s) w) (scc s).
 
+Definition spsr (s : state) (o : option exn_mode) : word := spsr (proc s) o.
+
 Definition set_spsr (s : state) (o : option exn_mode) (w : word) : state :=
   mk_state (set_spsr (proc s) o w) (scc s).
-
-Definition set_reg_of_mode (s : state) (m : proc_mode) (k : regnum) (w : word)
-  : state := mk_state (set_reg_of_mode (proc s) m k w) (scc s).
-
-Definition set_reg (s : state) (k : regnum) (w : word) : state :=
-  mk_state (set_reg (proc s) k w) (scc s).
 
 Definition reg_content_of_mode (s : state) (m : proc_mode) (k : regnum) : word
   := reg_content_of_mode (proc s) m k.
@@ -40,5 +38,15 @@ Definition reg_content_of_mode (s : state) (m : proc_mode) (k : regnum) : word
 Definition reg_content (s : state) (k : regnum) : word :=
   reg_content (proc s) k.
 
+Definition set_reg_of_mode (s : state) (m : proc_mode) (k : regnum) (w : word)
+  : state := mk_state (set_reg_of_mode (proc s) m k w) (scc s).
+
+Definition set_reg (s : state) (k : regnum) (w : word) : state :=
+  mk_state (set_reg (proc s) k w) (scc s).
+
+Definition exns (s : state) : list exception := exns (proc s).
+
 Definition set_exns (s : state) (es : list exception) : state :=
   mk_state (set_exns (proc s) es) (scc s).
+
+Definition mode (s : state) : proc_mode := mode (proc s).
