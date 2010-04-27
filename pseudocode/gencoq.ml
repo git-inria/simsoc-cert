2b7b3b37556c3f4421234e13de764ee04539f41d
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2010-04-27 04:07:58 +0000

    more code generation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@296 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- pseudocode/gencoq.ml
+++ pseudocode/gencoq.ml
@@ -161,59 +161,73 @@ let binop b s = string b (coq_binop s);;
 (***********************************************************************)
 
 (*REMOVE when finished! *)
-let todo_exp f b e = bprintf b "(*todo: %a*) (repr 0)" f e;;
+let todo_exp s b e = bprintf b "(*todo: %a*) %s" Genpc.exp e s;;
+let todo_word = todo_exp "(repr 0)";;
+let todo_bool = todo_exp "true";;
+
+let is_not_num = function
+  | Num _ -> false
+  | _ -> true;;
 
 let rec pexp b = function
   | Var _ as e -> exp b e
   | e -> par exp b e
 
-and pexp_num b = function
+and num_exp b = function
   | Num s -> num b s
   | e -> pexp b e
 
-and pexp_regnum b = function
+and regnum_exp b = function
   | Num s -> regnum b s
   | Var s -> string b s
   | e -> bprintf b "(mk_regnum %a)" pexp e
+
+and address b e = bprintf b "(mk_address %a)" pexp e
+
+and nat_exp b e = bprintf b "(nat_of_Z %a)" pexp e
 
 and exp b = function
   | Bin s -> word bin b s
   | Hex s -> word hex b s
   | Num s -> word num b s
-
-  | Var ("CP15_reg1_Ubit"|"CP15_reg1_EEbit" as s) ->
-      comment string b "FIXME"; comment string b s; string b "(repr 0)" (*FIXME*)
   | Var s -> string b s
 
+  | Fun ("Shared", _) as e -> (*FIXME*) todo_bool b e
+  | Fun (("CPSR_with_specified_E_bit_modification"|"TLB"|"ExecutingProcessor"
+	 |"accvalue"), _) as e -> (*FIXME*) todo_word b e
+
   | Fun (f, []) -> fun_name b f
-  | Fun (f, es) -> bprintf b "%a %a" fun_name f (list " " pexp_num) es
+  | Fun ("SignedSat"|"SignedDoesSat" as f, [e1; e2]) when is_not_num e2 ->
+      bprintf b "%a %a %a" fun_name f pexp e1 nat_exp e2
+  | Fun (f, es) -> bprintf b "%a %a" fun_name f (list " " num_exp) es
 
   | BinOp (e1, ("==" as f), Num n) -> (* avoid a repr *)
       bprintf b "%a %a %a" binop f pexp e1 num n
+(*REMOVE:
   | BinOp (Var "address" as e1, f, e2) -> (* avoid a typing problem in Coq *)
-      bprintf b "mk_address (%a %a %a)" binop f pexp e1 pexp e2 
+      bprintf b "mk_address (%a %a %a)" binop f pexp e1 pexp e2 *)
   | BinOp (e1, f, e2) -> bprintf b "%a %a %a" binop f pexp e1 pexp e2
 
   | If_exp (e1, e2, e3) ->
       bprintf b "if %a then %a else %a" exp e1 exp e2 exp e3
   | CPSR -> string b "cpsr s0"
   | Range (e, r) -> bprintf b "%a[%a]" pexp e range r
-  | Memory (e, n) -> bprintf b "read s0 %a %a" pexp e size n
+  | Memory (e, n) -> bprintf b "read s0 %a %a" address e size n
 
   | SPSR None -> string b "spsr s0 None"
   | SPSR (Some m) -> bprintf b "spsr s0 (Some %a)" exn_mode m
 
-  | Reg (e, None) -> bprintf b "reg_content s0 %a" pexp_regnum e
+  | Reg (e, None) -> bprintf b "reg_content s0 %a" regnum_exp e
   | Reg (e, Some m) ->
-      bprintf b "reg_content_mode s0 %a %a" pexp_regnum e mode m
+      bprintf b "reg_content_mode s0 %a %a" regnum_exp e mode m
 
-  | Coproc_exp (_, _, _) as e -> todo_exp Genpc.exp b e
+  | Coproc_exp (_, _, _) as e -> todo_word b e
 
   | Other _ | Unpredictable_exp | Unaffected -> invalid_arg "Gencoq.exp"
 
 and range b = function
   | Flag (s, _) -> bprintf b "%sbit" s
-  | Index e -> pexp_num b e
+  | Index e -> num_exp b e
   | Bits (n1, n2) -> bprintf b "%a#%a" num n1 num n2;;
 
 (***********************************************************************)
@@ -240,9 +254,16 @@ and inst_aux k b = function
   | If (e, i1, Some i2) ->
       bprintf b "if_then_else %a\n%a\n%a"
 	pexp e (pinst (k+2)) i1 (pinst (k+2)) i2
+(*  | Affect (Range (Var _ as e1, r), e2) ->
+      bprintf b "%a %a %a" affect_range r pexp e2 pexp e1*)
   | Affect (e1, e2) as i -> affect b i e2 e1
   | Proc _ | While _ | For _ | Coproc _ | Case _ as i -> todo (Genpc.inst 0) b i
   | Misc _ | Assert _ -> invalid_arg "Gencoq.inst"
+
+(*and affect_range b = function
+  | Bits (p, q) -> bprintf b "update_bits %a %a" num p num q
+  | Index e -> bprintf b "update_bit %a" num_exp e
+  | Flag (s, _) -> bprintf b "update_bit %sbit" s*)
 
 and affect b i v = function
   | Var s -> bprintf b "let %s := %a in" s exp v
@@ -251,15 +272,15 @@ and affect b i v = function
       with Not_found -> todo (Genpc.inst 0) b i end
 
 and affect_aux b = function
-  | Reg (e, None) -> bprintf b "set_reg %a" pexp_regnum e
-  | Reg (e, Some m) -> bprintf b "set_reg_mode %a %a" mode m pexp_regnum e
+  | Reg (e, None) -> bprintf b "set_reg %a" regnum_exp e
+  | Reg (e, Some m) -> bprintf b "set_reg_mode %a %a" mode m regnum_exp e
   | CPSR -> bprintf b "set_cpsr"
   | SPSR None -> bprintf b "set_spsr None"
   | SPSR (Some m) -> bprintf b "set_spsr (Some %a)" exn_mode m
   | Range (CPSR, Flag (s, _)) -> bprintf b "set_cpsr_bit %sbit" s
-  | Range (CPSR, Index e) -> bprintf b "set_cpsr_bit %a" pexp_num e
+  | Range (CPSR, Index e) -> bprintf b "set_cpsr_bit %a" num_exp e
   | Range (CPSR, Bits (n, p)) -> bprintf b "set_cpsr_bits %a %a" num n num p
-  | Memory (e, n) -> bprintf b "write %a %a" pexp e size n
+  | Memory (e, n) -> bprintf b "write %a %a" address e size n
   | _ -> raise Not_found;;
 
 (***********************************************************************)
@@ -338,24 +359,18 @@ For each instruction:
 semantics step function *)
 (***********************************************************************)
 
-(*REMOVE when finished!*)
-let exclude = function
-  | Instruction (_, id, _, _) ->
-      not (List.mem id.iname ["LDREX"])
-  | Operand (_, _, _, _) -> false;;
-
 let lib b ps =
   let btyp = Buffer.create 10000 in
   let bsem = Buffer.create 10000 in
   let prog_typ_sem p =
-    let p = lsm_hack p in
+    let p = lsm_hack p in (*FIXME*)
     let gs, ls = variables p in
       bprintf b "%a\n" (prog gs ls) p;
       bprintf btyp "%a\n" (inst_typ gs) p;
       bprintf bsem "%a\n" (inst_sem gs) p
   in
     bprintf b "Require Import Bitvec List Integers Util Functions Config Arm State Semantics.\nRequire Import ZArith.\nImport Int.\n\nModule Inst (Import C : CONFIG).\n\n";
-    List.iter prog_typ_sem (List.filter exclude ps); (*FIXME:remove exclude*)
+    List.iter prog_typ_sem ps;
     bprintf b "Inductive instruction : Type :=\n";
     Buffer.add_buffer b btyp;
     bprintf b "\nDefinition execute (w : word) (i : instruction) (s0 : state) : result :=\n  match i with\n";
