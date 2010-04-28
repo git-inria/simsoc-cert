82c2bdbace98541038ef43bdf1e47764e1ce47b2
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2010-04-28 07:57:09 +0000

    completely changed the options of the main
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@311 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- pseudocode/main.ml
+++ pseudocode/main.ml
@@ -19,69 +19,118 @@ open Lexing;;
 (** usage and exit function in case of error *)
 (***********************************************************************)
 
-let usage_msg () = "usage: " ^ Sys.argv.(0) ^ " [option] [file]";;
+let usage_msg() = "usage: " ^ Sys.argv.(0) ^ " [option ...]";;
 
-let print_usage_and_exit () = prerr_endline (usage_msg()); exit 1;;
+let print_usage_and_exit() = prerr_endline (usage_msg()); exit 1;;
 
-let error s = fprintf stderr "error: %s\n" s; print_usage_and_exit ();;
+let error s = fprintf stderr "error: %s\n" s; print_usage_and_exit();;
 
 (***********************************************************************)
-(** parameters *)
+(** generic functions for handling references *)
 (***********************************************************************)
 
-let get_filename, set_filename =
-  let filename = ref "" in
-    (fun () -> !filename),
-    (fun s -> if !filename = "" then filename := s
-     else error "wrong number of arguments");;
+let get_set init =
+  let r = ref init in
+    (fun () -> !r),
+    (fun v -> r := v);;
 
-let set_debug_mode () = let _ = Parsing.set_trace true in ();;
+let get_set_bool() =
+  let r = ref false in
+    (fun () -> !r),
+    (fun () -> r := true);;
 
-type action = GenPC | GenPCCheck | GenPre | GenCxx | GenCoq | GenCoqDec;;
+let is_set_get_set m init =
+  let r = ref init and s = ref false in
+    (fun () -> !s),
+    (fun () -> if !s then !r else error (sprintf "no %s provided" m)),
+    (fun v -> if !s then error (sprintf "%s already provided" m)
+              else (r := v; s := true));;
 
-let get_action, is_action_set, set_action =
-  let action = ref GenPC and is_set = ref false in
-    (fun () -> !action),
-    (fun () -> !is_set),
-    (fun a -> if !is_set then error "wrong number of options"
-     else begin action := a; is_set := true end);;
+(***********************************************************************)
+(** functions for setting parameters *)
+(***********************************************************************)
+
+let get_norm, set_norm = get_set_bool();;
+let get_check, set_check = get_set_bool();;
+let get_verbose, set_verbose = get_set_bool();;
+let get_debug, set_debug = get_set_bool();;
+
+let set_debug () =
+  let _ = Parsing.set_trace true in set_debug(); set_verbose();;
+
+let set_check () = set_check (); set_verbose();;
+
+type input_type = PCin | Decode;;
+type output_type = PCout | Cxx | Coq;;
+
+let is_set_input_type, get_input_type, set_input_type =
+  is_set_get_set "input type" PCin;;
+
+let is_set_input_file, get_input_file, set_input_file =
+  is_set_get_set "input file name" "";;
+
+let is_set_output_type, get_output_type, set_output_type =
+  is_set_get_set "output type" PCout;;
+
+let fverbose fmt f x =
+  if get_verbose() then fprintf stderr fmt f x else ();;
+
+let verbose x =
+  if get_verbose() then fprintf stderr "%s" x else ();;
 
 (***********************************************************************)
 (** command line parsing *)
 (***********************************************************************)
 
-let rec options () = [
-  "-h", Unit print_help, " Display this list of options";
-  "-d", Unit set_debug_mode, " Turn on debug mode";
-  "-pc", Unit (fun () -> set_action GenPC), " Generate pseudocode";
-  "-pc-check", Unit (fun () -> set_action GenPCCheck),
-  " Generate pseudocode and reparse it";
-  "-norm", Unit (fun () -> set_action GenPre),
-  " Generate normalized pseudocode";
-  "-cxx", Unit (fun () -> set_action GenCxx), " Generate instructions in C++";
-  "-coq", Unit (fun () -> set_action GenCoq), " Generate instructions in Coq";
-  "-coq-decoder", Unit (fun () -> set_action GenCoqDec),
-  " Generate decoder in Coq";
+let rec options() =
+  List.sort (fun (x,_,_) (y,_,_) -> Pervasives.compare x y)
+[
+  "-h", Unit print_help,
+  " Display this list of options";
+  "-d", Unit set_debug,
+  " Debugging mode";
+  "-ipc", String (fun s -> set_input_type PCin; set_input_file s),
+  " Take pseudocode instructions as input";
+  "-idec", String (fun s -> set_input_type Decode; set_input_file s),
+  " Take decoding instructions as input";
+  "-check", Unit set_check,
+  " Check pseudocode pretty-printer (with -ipc only)";
+  "-norm", Unit set_norm,
+  " Normalize pseudocode (with -ipc only)";
+  "-opc", Unit (fun () -> set_output_type PCout),
+  " Output pseudocode";
+  "-ocxx", Unit (fun () -> set_norm(); set_output_type Cxx),
+  " Output C++ (implies -norm)";
+  "-ocoq", Unit (fun () -> set_norm(); set_output_type Coq),
+  " Output Coq (implies -norm)";
+  "-v", Unit set_verbose,
+  " Verbose mode"
 ]
 
 and print_options oc () =
-  List.iter (fun (k, _, d) -> fprintf oc "%s: %s\n" k d) (options ())
+  List.iter (fun (k, _, d) -> fprintf oc "%s: %s\n" k d) (options())
 
 and print_help () =
   print_endline (usage_msg()); print_options stdout (); exit 0;;
 
+let anon_fun _ = error "invalid option";;
+
 let parse_args () =
-  Arg.parse (options()) set_filename (usage_msg());
-  if get_filename() = "" then error "no filename given";
-  if not (is_action_set()) then error "wrong number of option";;
+  Arg.parse (options()) anon_fun (usage_msg());
+  let _ = get_input_type() in
+  let _ = get_input_file() in
+  let _ = get_output_type() in
+    ();;
 
 (***********************************************************************)
 (** parsing functions *)
 (***********************************************************************)
 
 let fprint_loc oc loc =
-  Printf.fprintf oc "file \"%s\", line %d, character %d"
-    loc.pos_fname loc.pos_lnum (loc.pos_cnum - loc.pos_bol + 1);;
+  if loc.pos_fname <> "" then
+    fprintf oc "file \"%s\", " loc.pos_fname;
+  fprintf oc "line %d, character %d"
+    loc.pos_lnum (loc.pos_cnum - loc.pos_bol + 1);;
 
 let parse_lexbuf lb =
   try Parser.lib Lexer.token lb
@@ -90,7 +139,7 @@ let parse_lexbuf lb =
 
 let parse_channel ic =
   let lb = Lexing.from_channel ic in
-    lb.lex_curr_p <- { lb.lex_curr_p with pos_fname = get_filename() };
+    lb.lex_curr_p <- { lb.lex_curr_p with pos_fname = get_input_file() };
     parse_lexbuf lb;;
 
 let parse_string s = parse_lexbuf (Lexing.from_string s);;
@@ -104,37 +153,71 @@ let parse_file fn =
 (** main procedure *)
 (***********************************************************************)
 
-let print f x =
-  let b = Buffer.create 10000 in
-    f b x; Buffer.output_buffer stdout b;;
+type input =
+  | Prog of Ast.prog list
+  | Dec of Codetype.maplist;;
 
-let output gen = print gen (parse_file (get_filename()));;
+let get_input, set_input = get_set (Prog []);;
 
-let output_norm gen =
-  print gen (List.map Preproc.prog (parse_file (get_filename())));;
+let check() =
+  if get_check() then
+    match get_input() with
+      | Prog ps ->
+	  let b = Buffer.create 10000 in
+	    verbose "reparsing... ";
+	    Genpc.lib b ps;
+	    let ps' = parse_string (Buffer.contents b) in
+	      if ps = ps' then verbose "ok\n" else error "failed"
+      | Dec _ -> error "option -check incompatible with -idec";;
 
-let genpc_check () =
-  let b = Buffer.create 10000 in
-  let ps = parse_file (get_filename()) in
-    Genpc.lib b ps;
-    Buffer.output_buffer stdout b;
-    let ps' = parse_string (Buffer.contents b) in
-      fprintf stderr "reparsing: %s\n"
-	(if ps = ps' then "good" else "wrong");;
+let norm() =
+  if get_norm() then
+    match get_input() with
+      | Prog ps ->
+	  verbose "normalization... ";
+	  let ps = List.map Preproc.prog ps in
+	    if get_check() then
+	      let ps' = List.map Preproc.prog ps in
+		if ps = ps' then verbose "ok\n" else error "failed"
+	    else verbose "\n";
+	    set_input (Prog ps);
+	    check();
+      | Dec _ -> error "option -norm incompatible with -idec";;
 
-let main () =
-  parse_args ();
-  match get_action () with
-    | GenCoqDec ->
-	print Gencoqdec.decode (input_value (open_in (get_filename ())))
-    | GenPC -> output Genpc.lib
-    | GenPCCheck -> genpc_check ()
-    | GenPre -> output_norm Genpc.lib
-    | GenCxx -> output_norm Gencxx.lib
-    | GenCoq -> output_norm Gencoq.lib;;
+let parse_input_file() =
+  verbose "parsing...\n";
+  match get_input_type() with
+    | PCin ->
+	set_input (Prog (parse_file (get_input_file())));
+	check();
+	norm()
+    | Decode -> set_input (Dec (input_value (open_in (get_input_file()))));;
+
+let prog() =
+  match get_output_type() with
+    | PCout -> Genpc.lib
+    | Cxx -> Gencxx.lib
+    | Coq -> Gencoq.lib;;
+
+let dec() =
+  match get_output_type() with
+    | PCout -> error "option -opc incompatible with -idec"
+    | Cxx -> error "option -ocxx not yet supported with -idec"
+    | Coq -> Gencoqdec.decode;;
+
+let genr_output() =
+  verbose "code generation...\n";
+  match get_input() with
+    | Prog x -> Util.print (prog()) x
+    | Dec x -> Util.print (dec()) x;;
+
+let main() =
+  parse_args();
+  parse_input_file();
+  genr_output();;
 
 (***********************************************************************)
 (** launch the main procedure *)
 (***********************************************************************)
 
-let _ = main ();;
+let _ = main();;
