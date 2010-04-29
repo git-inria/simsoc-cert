72c0053341ac0ccc883a5eeaee1470f1eef1b5c9
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2010-04-29 04:26:29 +0000

    replace all English expressions by function calls, replace opcode[25] by (variable) opcode25, and replace mode by CPSR[4:0]
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@328 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- refARMparsing/preproc_pseudo.sh
+++ refARMparsing/preproc_pseudo.sh
@@ -6,17 +6,18 @@
 # permute comments and semi-colons
 # add missing "then" with some "if"
 # add "do" after "while" and "for"
-# in names, replace a dash by a space
+# replace dash by a space
 # replace "is" by "==", and "is not" by "!="
 # replace "== R15" by "== 15"
 # remove "value" of a register
 # replace some English expressions by a function call
-# fix English typos
+# fix some typos
+# inline some code
 # replace a strange dash character by a usual dash
-# remove the '<' and '>' around a "shift_imm"
 
-# REMARK: do not remove the last strange -e command !
-# due to a strange dash character
+
+# WARNING: do not remove the last strange -e command !
+# it is due to a strange dash character
 
 sed \
     -e '/^ *[a-zA-Z][a-zA-Z0-9 _(\+)]* = /s|$|;|' \
@@ -47,24 +48,36 @@ sed \
     -e 's|value to|to|' \
     -e 's|first value|first_value|' \
     -e 's|second value|second_value|' \
-    -e 's|v4 and earlier|v4_and_earlier|' \
-    -e 's|v5 and above|v5_and_above|' \
-    -e 's|architecture version 5 or above|v5_and_above|' \
-    -e 's|ARMv5 or above|v5_and_above|' \
-    -e 's|CPSR with|CPSR_with|' \
-    -e 's|address of|address_of|' \
     -e 's|dependent operation|dependent_operation|g' \
-    -e 's|SUB ARCHITECTURE|SUBARCHITECTURE|g' \
-    -e 's|not overridden|not_overridden|' \
+    -e 's|Mask|Mask()|' \
+    -e 's|CP15\(.*\)bit|CP15\1bit()|' \
+    -e 's|v4 and earlier|v4_and_earlier()|' \
+    -e 's|v5 and above|v5_and_above()|' \
+    -e 's|architecture version 5 or above|v5_and_above()|' \
+    -e 's|ARMv5 or above|v5_and_above()|' \
+    -e 's|IMPLEMENTATION DEFINED CONDITION|IMPLEMENTATION_DEFINED_CONDITION()|g' \
+    -e 's|SUB ARCHITECTURE DEFINED value|SUB_ARCHITECTURE_DEFINED_value()|g' \
+    -e 's|high vectors configured|high_vectors_configured()|' \
+    -e 's|(JE bit of Main Configuration register)|JE_bit_of_Main_Configuration_register()|' \
+    -e 's|(CV bit of Jazelle OS Control register)|CV_bit_of_Jazelle_OS_Control_register()|' \
+    -e 's|(Jazelle Extension accepts opcode at jpc)|Jazelle_Extension_accepts_opcode_at(jpc)|' \
+    -e 's|address of BKPT instruction|address_of_current_instruction()|' \
+    -e 's|address of the instruction after the BLX instruction|address_of_next_instruction()|' \
+    -e 's|address of instruction after the BLX instruction|address_of_next_instruction()|' \
+    -e 's|address of next instruction after the SWI instruction|address_of_next_instruction()|' \
+    -e 's|address of the instruction after the branch instruction|address_of_next_instruction()|' \
+    -e 's|CPSR with specified E bit modification|CPSR_with_specified_E_bit_modification()|' \
+    -e 's|(not overridden by debug hardware)|not_overridden_by_debug_hardware()|' \
     -e "s|bit position of most significant'1' in Rm|bit_position_of_most_significant_1(Rm)|" \
-    -e 's|Start opcode execution at \(.*\);|Start_opcode_execution_at(\1);|' \
+    -e 's|Start opcode execution at jpc|Start_opcode_execution_at(jpc)|' \
     -e 's|8_bit_immediate|immed_8|' \
     -e 's|coprocessor\[|Coprocessor[|' \
     -e 's|^\( *\)If |\1if |' \
     -e 's|Artihmetic|Arithmetic|' \
     -e 's|(diff4]|(diff4)|' \
     -e 's|memory\[|Memory\[|' \
-    -e 's|Data processing operands|Data Processing Operands|' \
     -e 's|<shift_imm>|shift_imm|' \
-    -e 's|See “Data Processing Operands - Rotate right with extend” on page A5-17|shifter_operand = (C Flag Logical_Shift_Left 31) OR (Rm Logical_Shift_Right 1); shifter_carry_out = Rm[0]|' \
+    -e 's|See “Data processing operands - Rotate right with extend” on page A5-17|shifter_operand = (C Flag Logical_Shift_Left 31) OR (Rm Logical_Shift_Right 1); shifter_carry_out = Rm[0]|' \
+    -e 's|opcode\[\(.*\)\]|opcode\1|' \
+    -e 's|mode;|CPSR[4:0];|' \
     -e 's|–|-|'
