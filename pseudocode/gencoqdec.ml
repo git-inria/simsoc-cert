4a3a1b225185a656b12843a1d3db486d593c5ac1
Xiaomu Shi <shi@users.gforge.inria.noreply>
2010-05-31 07:18:51 +0000

    add decode of addressing mode
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@410 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- pseudocode/gencoqdec.ml
+++ pseudocode/gencoqdec.ml
@@ -15,8 +15,8 @@ open Ast;;
 open Printf;;
 open Util;;
 open Codetype;;
-
-(*type lightheader = LH of int list * string
+(*
+type lightheader = LH of int list * string
 
 type pos_contents = 
   | Nothing
@@ -28,76 +28,246 @@ type pos_contents =
 
 type maplist = (lightheader * pos_contents array) list 
 *)
-
 let lightheader b p =
   match p with LH (is, s)
-  -> bprintf b "%a%s" (list "" int) is s;;
+  -> bprintf b "%a - %s" (list "." int) is s;;
 
 let comment b lh = bprintf b "(*%a*)" lightheader lh;;
 
-(*TODO : change the names of addressing mode instructions*)
-let inst_name b p =
-    match p with LH (_, s)
-    -> bprintf b "%s" s;;
+let str_to_lst s =
+  Str.split (Str.regexp "[-:<>() \t]+") s;;
 
-(*TODO : add bit x y*)
+let name_lst (lh,_) =
+  match lh with
+    | LH (_, "B, BL") -> ["B"]
+    | LH (_, s) ->
+	str_to_lst s;;
+
+let rec underscore = function
+  | [] -> ""
+  | [s] -> s
+  | s :: ss -> s ^ "_" ^ underscore ss;;
+
+type kind =
+  | Addr_mode of int
+  | Inst
+  | Encoding;;
+
+let add_mode ss =
+  match ss with
+    | "Encoding" :: _ -> Encoding
+    | "Data" :: _ -> Addr_mode 1
+    | "Miscellaneous" :: _ -> Addr_mode 3
+    | _ :: _ :: _ :: m :: _ ->
+	begin match m with
+	  | "Word" -> Addr_mode 2
+	  | "Multiple" -> Addr_mode 4
+	  | "Coprocessor" -> Addr_mode 5
+	  | _ -> Addr_mode 0
+	end
+    | _ -> Inst;;
+
+let short_name ss =
+  match ss with
+    | "Data" :: "processing" :: "operands" :: s -> s
+    | "Load" :: "and" :: "Store" :: "Word" :: "or" :: "Unsigned" :: "Byte" :: s -> s
+    | "Miscellaneous" :: "Loads" :: "and" :: "Stores" :: s -> s
+    | "Load" :: "and" :: "Store" :: "Multiple" :: s -> s
+    | "Load" :: "and" :: "Store" :: "Coprocessor" :: s -> s
+    | _ -> ss;; 
+
+
+let name ss =
+  let md = add_mode ss in
+    match md with
+      | Addr_mode i -> (Printf.sprintf "M%d" i) :: (short_name ss)
+      | Inst -> ss
+      | Encoding -> ss;;
+
+let id ps =  underscore (name (name_lst ps));;
+
+let pos_info pc =
+  let ar = Array.create (Array.length pc) (Nothing, 0) in
+    for i = 0 to Array.length pc - 1 do
+      ar.(i) <- (pc.(i), i)
+    done;
+    ar;;
+
 let dec b pc =
-    match pc with
-      | Value s -> 
-	  begin match s with
-	    | true -> bprintf b "1"
-	    | false -> string b "0"
-	  end 
-      | Shouldbe s -> 
-	  begin match s with
-	    | true -> string b "1"
-	    | false -> string b "0"
-	  end
-      | Param1 s -> bprintf b "%c" s
-      | Param1s _ -> string b "_"
-      | Range _ -> string b "_"
-      | Nothing -> ();;
+  match pc with
+    | (Value s, _) -> 
+	begin match s with
+	  | true -> string b "1"
+	  | false -> string b "0"
+	end 
+    | (Shouldbe s, i) -> 
+	begin match s with
+	  | true -> bprintf b "SBO%d" i 
+	  | false -> bprintf b "SBZ%d" i
+	end
+    | (Param1 c, _) -> bprintf b "%s"  (Char.escaped c)
+    | (Param1s s, _) -> bprintf b "%s" s
+    | (Range _, _) -> string b "_"
+    | (Nothing, _) -> ()
+;;
 
-(*TODO : arguments order*)
-(*TODO : bit 'I' is not an argument in data processing instructions*)
-(*TODO : blank between arguments*)
+let mode_var m lst =
+  let l = List.map (fun (s,_,_) -> s) lst in
+  let n = List.hd m in
+  let mode3 = ["LDRD";"LDRH";"LDRSB";"LDRSH";"STRD";"STRH"] in
+  let mode4 = ["RFE";"SRS"] in
+  let mode5 = ["LDC";"STC"] in
+    if (List.mem "shifter_operand" l) then 1
+    else if (List.mem "register_list" l) then 4
+    else if (List.mem "addr_mode" l) then 
+      if (List.mem n mode3) then 3 else 2
+    else if (List.mem n mode4) then 4
+    else if (List.mem n mode5) then 5
+    else 0;;
 
-let inst_param b ps =
-    match ps with
-      | (Range (("Rs"| "Rm" | "Rn" | "Rd" | "CRd" | "CRm"| "CRn"),_ ,_), i) ->
-	  bprintf b "(reg_num_from bit %d w)" i
-      | (Range ("shifter_operand", _, _), _) -> 
-	  string b "(decode_shifter_operand w I y)"
-      | (Range (("immed_24"| "signed_immed_24"), _, _), _) ->
-	  string b "(signed_immed_24 w)"
-      | (Range ("cond", _, _), _) -> string b "(cond w)"
-      | (Range (_, i1, _), i) -> bprintf b "w[%d#%d]" (i+i1-1) i
-      | (Param1s s, _) -> bprintf b "%s " s
-      | (Param1 c, _) -> bprintf b "%c" c
-      | ((Value _ | Shouldbe _ | Nothing) , _)
-	  -> ()
- ;;
+let add_mode_var n lst =
+    if (mode_var n lst != 0) then
+      let v = Printf.sprintf "(decode_addr_mode%d w)" (mode_var n lst) in
+	List.append [(v, 0, 0)] lst
+    else lst;;
+
+let not_var1 i =
+  match i with
+    | 1 -> ["S"; "cond"; "d"; "n"; "opcode"]
+    | 2 -> ["B"; "L"; "cond"; "d"; "H"; "S"]
+    | 3 -> ["B"; "L"; "cond"; "d"; "H"; "S"]
+    | 4 -> ["L"; "S"; "CRd"; "N"; "option"]
+    | 5 -> ["L"; "U"; "S" ; "CRd"; "N"; "option"]
+    | _ -> [];;
+
+let not_var2 i =
+  match i with
+    | 1 -> ["shifter_operand"; "I"]
+    | 2 -> ["P"; "U"; "W"; "addr_mode"]
+    | 3 -> ["I"; "P"; "W"; "U"; "Rn"; "addr_mode"]
+    | 4 -> ["P"; "U"; "W"; "Rn"]
+    | 5 -> ["8_bit_word_offset"; "CRd"; "P"; "U"; "W"]
+    | _ -> [];;
+
+let not_var n =
+    match n with
+      | ("MRC"|"CDP")::_ -> ["opcode_1"; "opcode_2"; "CRd"; "CRm"; "CRn"]
+      | _ -> [];;
+
+let is_not_var1 i =
+  fun (s, _, _) -> List.mem s (not_var1 i);;
+
+let is_not_var2 i =
+  fun (s, _, _) -> List.mem s (not_var2 i);;
+
+let remove_var n lst = 
+  let im = mode_var n lst in
+  let md = add_mode n in
+    List.map (fun s -> if (
+		match md with
+		  | Addr_mode i -> is_not_var1 i s
+		  | Inst -> is_not_var2 im s
+		  | Encoding -> false) then
+		("",0,0) else s) lst;;
+
+let remove_vars n lst =
+  let md = add_mode n in
+    match md with
+      | (Addr_mode _ | Inst) -> remove_var n lst
+      | Encoding -> lst;;
+
+let inst_param ls =
+  match ls with
+    | (("s" | "m" | "n" | "d" | "dHi" | "dLo"), i, _) ->
+	Printf.sprintf "(reg_num_from bit %d w)" i
+    | ("shifter_operand", _, _) ->
+	"(decode_shifter_operand w I x)"
+    | ("cond", _, _) ->
+	"(condition w)"
+    | (s, p, l) -> 
+	if l > 1 then 
+	  Printf.sprintf "w[%d#%d]" (p+l-1) p
+	else
+	  Printf.sprintf "%s" s
+;;
 
 let param_m ar =
-  let res = Array.create (Array.length ar) (Nothing,0) in
+  let res = Array.create (Array.length ar) ("", 0, 0) in
     for i = 0 to (Array.length ar -1) do
-      res.(i) <- (ar.(i), i)
+      match ar.(i) with
+	| Range (s, len, _) ->
+	    if s.[0] = 'R' then
+	      res.(i) <- ((String.sub s 1 (String.length s -1)), i, len)
+	    else
+	      res.(i) <- (s, i, len)
+	| (Nothing | Value _ | Shouldbe _) -> 
+	    res.(i) <- ("", 0, 0)
+	| Param1 c -> 
+	    res.(i) <-  ((Char.escaped c), i, 1)
+	| Param1s s -> 
+	    res.(i) <- (s, i, 1)
     done;
     for i = 0 to (Array.length ar -1) do
-      match res.(i) with
-	| (Range (_, len, _),_) ->
-	    for j = 1 to len-1 do
-	      res.(i+j) <- (Nothing, i+j)
-	    done;
-	| ((Param1 _ | Value _ | Shouldbe _ | Nothing | Param1s _),_) ->
-	    res.(i) <- res.(i)
-    done;
+    match res.(i) with
+      | ("immed", _, _) ->
+	  res.(i) <- ("", 0, 0)
+      | ("I", 25, _) -> 
+	  res.(i) <- ("", 0, 0)
+      | (_, _, len) ->
+	  if len > 0 then
+	  for j = 1 to len -1 do
+	    res.(i + j) <- ("", 0, 0)
+	  done;
+	  done;
     res;;
 
-let params f x =
+let params f (lh, ls) =
+  let dname = name (name_lst (lh,ls)) in
+  let n = name_lst (lh, ls) in
   let aux b =
-    let lst = Array.to_list (param_m x) in
-      (list "" f) b lst
+    let lst =
+	(List.filter ((<>) "")
+	      (List.map inst_param
+		 (remove_vars n
+		    (add_mode_var dname
+		       (List.sort (fun (s1, _, _) (s2, _, _) -> 
+				     Pervasives.compare s1 s2)
+			  (Array.to_list (param_m ls)))))))
+    in
+      (list " " f) b lst
+  in aux;;
+
+let sbo_tst ls =
+  match ls with
+    | (Shouldbe true, i) -> Printf.sprintf "SBO%d" i
+    | ((Nothing | Value _ | Param1 _ | Param1s _ | Range _ | Shouldbe false), _)
+      -> "";;
+
+let sbz_tst ls =
+  match ls with
+    | (Shouldbe false, i) -> Printf.sprintf "SBZ%d" i
+    | ((Nothing | Value _ | Param1 _ | Param1s _ | Range _ | Shouldbe true), _)
+      -> "";;
+
+let shouldbe_test (lh, ls) =
+  let lst = Array.to_list ls in
+  let ps = Array.to_list (pos_info ls) in
+  let sbo = List.filter ((<>) "") (List.map sbo_tst ps) in
+  let sbz = List.filter ((<>) "") (List.map sbz_tst ps) in
+  let aux b =
+    if ((List.mem (Shouldbe true) lst) && (not (List.mem (Shouldbe false) lst))) then
+      bprintf b "if (%a) then\n      DecInst (%s %t)\n      else Unpredictable"
+	(list "&&" string) sbo (id (lh,ls)) (params string (lh, ls))
+    else 
+      if (List.mem (Shouldbe false) lst && (not (List.mem (Shouldbe true) lst))) then
+	bprintf b "if (not (%a)) then \n      DecInst (%s %t)\n      else Unpredictable"
+	  (list "&&" string) sbz (id (lh,ls)) (params string (lh, ls))
+      else 
+	if (List.mem (Shouldbe false) lst && (List.mem (Shouldbe true) lst)) then
+	  bprintf b "if ((%a) && (not (%a))) then \n      DecInst (%s %t)\n      else Unpredictable"
+	 (list "&&" string) sbo (list "&" string) sbz (id (lh,ls)) (params string (lh, ls))
+	else
+	  bprintf b "DecInst (%s %t)" (id (lh,ls)) (params string (lh, ls))
   in aux;;
 
 let bits f x =
@@ -105,13 +275,48 @@ let bits f x =
     let lst = Array.to_list x in
       (list " '" f) b (List.rev lst)
   in aux;;
-    
-let dec_inst b (lh, ls) =
-  bprintf b
-    "    %a\n    | %t =>\n      Inst (%a %t)\n"
-    comment lh (bits dec ls) inst_name lh (params inst_param ls);
-;;
-(*TODO : add the definition begin and end*)
 
-let decode =
-  list "" dec_inst;;
+let dec_inst b (lh, ls) =
+  let dbits = pos_info ls in
+    let md = add_mode (name_lst (lh,ls)) in
+      match md with
+	| Inst -> 
+	    bprintf b "    %a\n    | %t =>\n      %t\n"
+	      comment lh (bits dec dbits) (shouldbe_test (lh, ls))
+	| Encoding -> ()
+	| Addr_mode _ -> 
+	    bprintf b "    %a\n    | %t =>\n      %s %t\n"
+	      comment lh (bits dec dbits)
+	      (id (lh, ls)) (params string (lh, ls))
+;;
+
+let is_inst l =
+  if ((add_mode (name_lst l)) = Inst) then true else false;;
+
+let is_addr_mode1 l =
+  if ((add_mode (name_lst l)) = Addr_mode 1) then true else false;;
+let is_addr_mode2 l =
+  if ((add_mode (name_lst l)) = Addr_mode 2) then true else false;;
+let is_addr_mode3 l =
+  if ((add_mode (name_lst l)) = Addr_mode 3) then true else false;;
+let is_addr_mode4 l =
+  if ((add_mode (name_lst l)) = Addr_mode 4) then true else false;;
+let is_addr_mode5 l =
+  if ((add_mode (name_lst l)) = Addr_mode 5) then true else false;;
+
+
+let decode b ps =
+  bprintf b "Definition decode_addr_mode1 (w : word) : mode1 :=\n match w with\n";
+  (list "" dec_inst) b (List.filter (is_addr_mode1) ps);
+  bprintf b "\n\nDefinition decode_addr_mode2 (w : word) : mode2 :=\n match w with\n";
+  (list "" dec_inst) b (List.filter (is_addr_mode2) ps);
+  bprintf b "\n\nDefinition decode_addr_mode3 (w : word) : mode3 :=\n match w with\n";
+  (list "" dec_inst) b (List.filter (is_addr_mode3) ps);
+  bprintf b "\n\nDefinition decode_addr_mode4 (w : word) : mode4 :=\n match w with\n";
+  (list "" dec_inst) b (List.filter (is_addr_mode4) ps);
+  bprintf b "\n\nDefinition decode_addr_mode5 (w : word) : mode5 :=\n match w with\n";
+  (list "" dec_inst) b (List.filter (is_addr_mode5) ps);
+  bprintf b "\n\nDefinition decode (w : word) : decode_result :=\n  match bools_of_word w with\n";
+  (list "" dec_inst) b (List.filter (is_inst) ps);
+  bprintf b "    | _ -> Unpredictable\n  end."
+;;
