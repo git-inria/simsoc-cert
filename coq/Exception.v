e28315c87ca273ddc09e4fd605a5718c427ce512
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2010-06-01 10:29:26 +0000

    completely redefined from the generated file arm6exn.v
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@465 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- coq/Exception.v
+++ coq/Exception.v
@@ -8,224 +8,136 @@ ARM Architecture Reference Manual, Issue I, July 2005.
 
 Page numbers refer to ARMv6.pdf.
 
-Exception handling.
-*)
+Exception handling. Step functions are taken from the generated file
+arm6/arm6exn.v. *)
 
-Require Import List State Arm.
+Require Import Config List ZArith Bitvec Arm SCC State Semantics Util String.
 
-(****************************************************************************)
-(* A2.6.2 Reset (p. 56) *)
-(****************************************************************************)
+Module InstSem (Import C : CONFIG).
 
-(* <<
-R14_svc = UNPREDICTABLE value
-SPSR_svc = UNPREDICTABLE value
-CPSR[4:0] = 0b10011 /* Enter Supervisor mode */
-CPSR[5] = 0 /* Execute in ARM state */
-CPSR[6] = 1 /* Disable fast interrupts */
-CPSR[7] = 1 /* Disable normal interrupts */
-CPSR[8] = 1 /* Disable Imprecise Aborts (v6 only) */
-CPSR[9] = CP15_reg1_EEbit /* Endianness on exception entry */
-if high vectors configured then
-  PC = 0xFFFF0000
-else
-  PC = 0x00000000
->>*)
+(* A2.6.2 Reset *)
+Definition Reset_step (s0 : state) : result :=
+  block (
+    set_reg_mode (exn svc) LR (repr 0) ::
+    let SPSR_svc := repr 0 in
+    set_cpsr (set_bits 4 0 (repr (Zpos 1~0~0~1~1)) (cpsr s0)) ::
+    set_cpsr (set_bit 5 (repr 0) (cpsr s0)) ::
+    set_cpsr (set_bit 6 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 7 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 8 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 9 ((CP15_reg1 s0)[EEbit]) (cpsr s0)) ::
+    if_then_else C.high_vectors_configured
+      (set_reg PC (repr (Zpos 4294901760)))
+      (set_reg PC (repr (Z0))) ::
+    nil) true s0.
 
-Definition handle_Reset (s : state) : option state := Some s.
+(* A2.6.3 UndIns *)
+Definition UndIns_step (s0 : state) : result :=
+  block (
+    set_reg_mode (exn und) LR (address_of_next_instruction s0) ::
+    let SPSR_und := cpsr s0 in
+    set_cpsr (set_bits 4 0 (repr (Zpos 1~1~0~1~1)) (cpsr s0)) ::
+    set_cpsr (set_bit 5 (repr 0) (cpsr s0)) ::
+    set_cpsr (set_bit 7 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 9 ((CP15_reg1 s0)[EEbit]) (cpsr s0)) ::
+    if_then_else C.high_vectors_configured
+      (set_reg PC (repr (Zpos 4294901764)))
+      (set_reg PC (repr (Zpos 4))) ::
+    nil) true s0.
 
-(*Definition handle_Reset (s : state) (m : processor_mode) : option state :=
-  Some s. (*FIXME*)*)
-  (*interp s m
-  (Seq (Affect (Reg_exn svc w14) All w0) (*FIXME*)
-  (Seq (Affect (SPSR svc) All w0) (*FIXME*)
-  (Seq (Affect CPSR (Bits 4 0) (W 1~0~0~1~1))
-  (Seq (Affect CPSR (Bit 5) w0)
-  (Seq (Affect CPSR (Bit 6) w1)
-  (Seq (Affect CPSR (Bit 7) w1)
-  (Seq (Affect CPSR (Bit 8) w1)
-  (Seq (Affect CPSR (Bit 9) w0) (*FIXME*)
-  (Affect (Reg w15) All w0))))))))). (*FIXME*)*)
+(* A2.6.4 SoftInt *)
+Definition SoftInt_step (s0 : state) : result :=
+  block (
+    set_reg_mode (exn svc) LR (address_of_next_instruction s0) ::
+    let SPSR_svc := cpsr s0 in
+    set_cpsr (set_bits 4 0 (repr (Zpos 1~0~0~1~1)) (cpsr s0)) ::
+    set_cpsr (set_bit 5 (repr 0) (cpsr s0)) ::
+    set_cpsr (set_bit 7 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 9 ((CP15_reg1 s0)[EEbit]) (cpsr s0)) ::
+    if_then_else C.high_vectors_configured
+      (set_reg PC (repr (Zpos 4294901768)))
+      (set_reg PC (repr (Zpos 8))) ::
+    nil) true s0.
 
-(****************************************************************************)
-(* A2.6.3 Undefined Instruction exception (p. 57) *)
-(****************************************************************************)
+(* A2.6.5 PFAbort *)
+Definition PFAbort_step (s0 : state) : result :=
+  block (
+    set_reg_mode (exn abt) LR (add (address_of_current_instruction s0) (repr 4)) ::
+    let SPSR_abt := cpsr s0 in
+    set_cpsr (set_bits 4 0 (repr (Zpos 1~0~1~1~1)) (cpsr s0)) ::
+    set_cpsr (set_bit 5 (repr 0) (cpsr s0)) ::
+    set_cpsr (set_bit 7 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 8 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 9 ((CP15_reg1 s0)[EEbit]) (cpsr s0)) ::
+    if_then_else C.high_vectors_configured
+      (set_reg PC (repr (Zpos 4294901772)))
+      (set_reg PC (repr (Zpos 12))) ::
+    nil) true s0.
 
-(* <<
-R14_und = address of next instruction after the Undefined instruction
-SPSR_und = CPSR
-CPSR[4:0] = 0b11011 /* Enter Undefined Instruction mode */
-CPSR[5] = 0 /* Execute in ARM state */
-/* CPSR[6] is unchanged */
-CPSR[7] = 1 /* Disable normal interrupts */
-/* CPSR[8] is unchanged */
-CPSR[9] = CP15_reg1_EEbit /* Endianness on exception entry */
-if high vectors configured then
-  PC = 0xFFFF0004
-else
-  PC = 0x00000004
->>*)
+(* A2.6.6 DataAbort *)
+Definition DataAbort_step (s0 : state) : result :=
+  block (
+    set_reg_mode (exn abt) LR (add (address_of_current_instruction s0) (repr 8)) ::
+    let SPSR_abt := cpsr s0 in
+    set_cpsr (set_bits 4 0 (repr (Zpos 1~0~1~1~1)) (cpsr s0)) ::
+    set_cpsr (set_bit 5 (repr 0) (cpsr s0)) ::
+    set_cpsr (set_bit 7 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 8 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 9 ((CP15_reg1 s0)[EEbit]) (cpsr s0)) ::
+    if_then_else C.high_vectors_configured
+      (set_reg PC (repr (Zpos 4294901776)))
+      (set_reg PC (repr (Zpos 16))) ::
+    nil) true s0.
 
-Definition handle_UndIns (s : state) : option state := Some s.
+(* A2.6.8 IRQ *)
+Definition IRQ_step (s0 : state) : result :=
+  block (
+    set_reg_mode (exn irq) LR (add (address_of_next_instruction s0) (repr 4)) ::
+    let SPSR_irq := cpsr s0 in
+    set_cpsr (set_bits 4 0 (repr (Zpos 1~0~0~1~0)) (cpsr s0)) ::
+    set_cpsr (set_bit 5 (repr 0) (cpsr s0)) ::
+    set_cpsr (set_bit 7 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 8 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 9 ((CP15_reg1 s0)[EEbit]) (cpsr s0)) ::
+    if_then_else (zeq ((CP15_reg1 s0)[VEbit]) 0)
+      (if_then_else C.high_vectors_configured
+        (set_reg PC (repr (Zpos 4294901784)))
+        (set_reg PC (repr (Zpos 24))))
+      (set_reg PC (VE_IRQ_address)) ::
+    nil) true s0.
 
-(*Definition handle_UndIns (s : state) (m : processor_mode) : option state :=
-  Some s. (*FIXME*)*)
-  (*interp s m
-  (Seq (Affect (Reg_exn und w14) All (next_inst_address s m)) (*FIXME?*)
-  (Seq (Affect (SPSR svc) All (State CPSR All)) (*FIXME*)
-  (Seq (Affect CPSR (Bits 4 0) (W 1~1~0~1~1))
-  (Seq (Affect CPSR (Bit 5) w0)
-  (Seq (Affect CPSR (Bit 7) w1)
-  (Seq (Affect CPSR (Bit 9) w0) (*FIXME*)
-  (Affect (Reg w15) All w4))))))). (*FIXME*)*)
+(* A2.6.9 FIQ *)
+Definition FIQ_step (s0 : state) : result :=
+  block (
+    set_reg_mode (exn fiq) LR (add (address_of_next_instruction s0) (repr 4)) ::
+    let SPSR_fiq := cpsr s0 in
+    set_cpsr (set_bits 4 0 (repr (Zpos 1~0~0~0~1)) (cpsr s0)) ::
+    set_cpsr (set_bit 5 (repr 0) (cpsr s0)) ::
+    set_cpsr (set_bit 6 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 7 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 8 (repr 1) (cpsr s0)) ::
+    set_cpsr (set_bit 9 ((CP15_reg1 s0)[EEbit]) (cpsr s0)) ::
+    if_then_else (zeq ((CP15_reg1 s0)[VEbit]) 0)
+      (if_then_else C.high_vectors_configured
+        (set_reg PC (repr (Zpos 4294901788)))
+        (set_reg PC (repr (Zpos 28))))
+      (set_reg PC (VE_FIQ_address)) ::
+    nil) true s0.
 
-(****************************************************************************)
-(* A2.6.4 Software Interrupt exception (p. 58) *)
-(****************************************************************************)
-
-(* <<
-R14_svc = address of next instruction after the SWI instruction
-SPSR_svc = CPSR
-CPSR[4:0] = 0b10011 /* Enter Supervisor mode */
-CPSR[5] = 0 /* Execute in ARM state */
-/* CPSR[6] is unchanged */
-CPSR[7] = 1 /* Disable normal interrupts */
-/* CPSR[8] is unchanged */
-CPSR[9] = CP15_reg1_EEbit /* Endianness on exception entry */
-if high vectors configured then
-  PC = 0xFFFF0008
-else
-  PC = 0x00000008
->>*)
-
-Definition handle_SoftInt (s : state) : option state := Some s.
-
-(*Definition handle_SoftInt (s : state) (m : processor_mode) : option state :=
-  Some s. (*FIXME*)*)
-
-(****************************************************************************)
-(* A2.6.5 Prefetch Abort (instruction fetch memory abort) (p. 58) *)
-(****************************************************************************)
-
-(* <<
-R14_abt = address of the aborted instruction + 4
-SPSR_abt = CPSR
-CPSR[4:0] = 0b10111 /* Enter Abort mode */
-CPSR[5] = 0 /* Execute in ARM state */
-/* CPSR[6] is unchanged */
-CPSR[7] = 1 /* Disable normal interrupts */
-CPSR[8] = 1 /* Disable Imprecise Data Aborts (v6 only) */
-CPSR[9] = CP15_reg1_EEbit /* Endianness on exception entry */
-if high vectors configured then
-  PC = 0xFFFF000C
-else
-  PC = 0x0000000C
->>*)
-
-Definition handle_PFAbort (s : state) : option state := Some s.
-
-(*Definition handle_PFAbort (s : state) (m : processor_mode) : option state :=
-  Some s. (*FIXME*)*)
-
-(****************************************************************************)
-(* A2.6.6 Data Abort (data access memory abort) (p. 59) *)
-(****************************************************************************)
-
-(* <<
-R14_abt = address of the aborted instruction + 8
-SPSR_abt = CPSR
-CPSR[4:0] = 0b10111 /* Enter Abort mode */
-CPSR[5] = 0 /* Execute in ARM state */
-/* CPSR[6] is unchanged */
-CPSR[7] = 1 /* Disable normal interrupts */
-CPSR[8] = 1 /* Disable Imprecise Data Aborts (v6 only) */
-CPSR[9] = CP15_reg1_EEbit /* Endianness on exception entry */
-if high vectors configured then
-  PC = 0xFFFF0010
-else
-  PC = 0x00000010
->>*)
-
-Definition handle_DataAbort (s : state) : option state := Some s.
-
-(*Definition handle_DataAbort (s : state) (m : processor_mode) : option state :=
-  Some s. (*FIXME*)*)
-
-(****************************************************************************)
-(* A2.6.7 Imprecise data aborts (p. 61) *)
-(****************************************************************************)
-
-Definition handle_ImpAbort (s : state) : option state := Some s.
-
-(*Definition handle_ImpAbort (s : state) (m : processor_mode) : option state :=
-  Some s. (*FIXME*)*)
-
-(****************************************************************************)
-(* A2.6.8 Interrupt request (IRQ) exception (p. 62) *)
-(****************************************************************************)
-
-(* <<
-R14_irq = address of next instruction to be executed + 4
-SPSR_irq = CPSR
-CPSR[4:0] = 0b10010 /* Enter IRQ mode */
-CPSR[5] = 0 /* Execute in ARM state */
-/* CPSR[6] is unchanged */
-CPSR[7] = 1 /* Disable normal interrupts */
-CPSR[8] = 1 /* Disable Imprecise Data Aborts (v6 only) */
-CPSR[9] = CP15_reg1_EEbit /* Endianness on exception entry */
-if VE==0 then
-  if high vectors configured then
-    PC = 0xFFFF0018
-  else
-    PC = 0x00000018
-else
-  PC = IMPLEMENTATION DEFINED /* see page A2-26 (p. 64) */
->>*)
-
-Definition handle_IRQ (s : state) : option state := Some s.
-
-(*Definition handle_IRQ (s : state) (m : processor_mode) : option state :=
-  Some s. (*FIXME*)*)
-
-(****************************************************************************)
-(* A2.6.9 Fast interrupt request (FIQ) exception (p. 62) *)
-(****************************************************************************)
-
-(* <<
-R14_fiq = address of next instruction to be executed + 4
-SPSR_fiq = CPSR
-CPSR[4:0] = 0b10001 /* Enter FIQ mode */
-CPSR[5] = 0 /* Execute in ARM state */
-CPSR[6] = 1 /* Disable fast interrupts */
-CPSR[7] = 1 /* Disable normal interrupts */
-CPSR[8] = 1 /* Disable Imprecise Data Aborts (v6 only) */
-CPSR[9] = CP15_reg1_EEbit /* Endianness on exception entry */
-if VE==0 then
-  if high vectors configured then
-    PC = 0xFFFF001C
-  else
-    PC = 0x0000001C
-else
-  PC = IMPLEMENTATION DEFINED /* see page A2-26 (p. 64) */
->>*)
-
-Definition handle_FIQ (s : state) : option state := Some s.
-
-(*Definition handle_FIQ (s : state) (m : processor_mode) : option state :=
-  Some s. (*FIXME*)*)
-
-Definition handle_exception (s : state) : option state :=
+Definition step (s : state) : result :=
   match exns s with
-    | nil => Some s
+    | nil => Ok false s
     | e :: _ =>
       match e with
-        | Reset => handle_Reset s
-        | UndIns => handle_UndIns s
-        | SoftInt => handle_SoftInt s
-        | PFAbort => handle_PFAbort s
-        | DataAbort => handle_DataAbort s
-        | ImpAbort => handle_ImpAbort s
-        | IRQ => handle_IRQ s
-        | FIQ => handle_FIQ s
+        | Reset => Reset_step s
+        | UndIns => UndIns_step s
+        | SoftInt => SoftInt_step s
+        | PFAbort => PFAbort_step s
+        | DataAbort => DataAbort_step s
+        | ImpAbort => todo "Imprecise data abort" false s
+        | IRQ => IRQ_step s
+        | FIQ => FIQ_step s
       end
   end.
+
+End InstSem.
