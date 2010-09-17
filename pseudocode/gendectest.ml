2d150796b062ed02cd51a1bfd6af0736c0bfc9f6
Xiaomu Shi <shi@users.gforge.inria.noreply>
2010-09-17 04:24:18 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@637 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- pseudocode/gendectest.ml
+++ pseudocode/gendectest.ml
@@ -56,7 +56,7 @@ type vconstraint =
   (*| NotZero of int  (* parameter that should not be zero *)
   | NotOne of int*)
   | NoWritebackDest    (* no write-back with Rd==Rn *)
-  | NotSame of string * string (* R<a> <> R<b> *)
+  | NotSame of int * int (* R<a> <> R<b> *)
   | NotLSL0            (* to distinguished between (equivalent?) mode cases *)
   | OtherVC of exp     (* Other validy constraints described by a boolean
                         * expression *)
@@ -84,45 +84,53 @@ let gen_unpred (lh, ls) =
       | "CPS"::_ -> Or (And (NotV ("mmod",Int32.zero,18), NotVs ("immod", 0b00,16)), 
 			Or (And (NotV ("mmod",Int32.zero,18), NotVs ("immod", 0b01,16)), 
 			    And (NotV ("mmod",Int32.one,18), NotVs ("immod", 0b01,16))))
-      | ("LDM1"|"LDM2") ::_ -> And (NotPC ("n", 16), NotZero 0)
+      | ("LDM1"|"LDM2"|"STM1"|"STM2") ::_ -> And (NotPC ("n", 16), NotZero 0)
       | "LDM3" :: _ -> NotPC ("n", 16)
-      | "LDR" :: _ -> NoWritebackDest
+      | ("LDR"|"STR"|"STRB") :: _ -> NoWritebackDest
       | "LDRB" :: _ -> NotPC ("n", 16)
-      | "LDRBT" :: _ -> Or (NotPC ("n", 16), NotSame ("n", "d"))
+      | "LDRBT" :: _ -> Or (NotPC ("n", 16), NotSame (16, 12))
       (*| "LDRD" :: _ -> *)
       | "LDREX" :: _ -> Or (NotPC ("n", 16), NotPC ("d", 12))
-      | "LDRH" :: _ -> Or (NotPC ("d", 12), NoWritebackDest)
-      | "LDRSB" :: _ -> Or (NotPC ("d", 12), NoWritebackDest)
-      | "LDRSH" :: _ -> Or (NotPC ("d", 12), NoWritebackDest)
-      | "LDRT" :: _ -> Or (NotPC ("d", 12), NotSame ("d", "n"))
+      | ("LDRH"|"LDRSB"|"LDRSH"|"STRH") :: _ -> Or (NotPC ("d", 12), NoWritebackDest)
+      | ("LDRT"|"STRBT") :: _ -> Or (NotPC ("d", 12), NotSame (16, 12))
       | "MCR" :: _ -> NotPC ("d", 12)
       | "MCRR" :: _ -> NotPC ("d" , 12)
-      | ("MLA"|"SMLAxy"|"SMLAWy"|"SMLSD"|"") :: _  -> 
+      | ("MLA"|"SMLAxy"|"SMLAWy"|"SMLSD"|"SMMLS") :: _  -> 
 	  Or (NotPC ("d", 16), Or (NotPC ("m", 0), Or (NotPC ("s", 8), NotPC ("n", 12))))
-      | "MRRC" :: _ -> Or (NotSame ("d", "n"), Or (NotPC ("d", 12), NotPC ("n", 16)))
+      | "MRRC" :: _ -> Or (NotSame (12, 16), Or (NotPC ("d", 12), NotPC ("n", 16)))
       | "MRS" :: _ -> NotPC ("d", 12)
       | "MUL" :: _ -> Or (NotPC ("d", 16), Or (NotPC ("s", 8), NotPC ("m",0)))
-      | ("PKHBT"|"PKHTB"|"QADD"|"QADD8"|"QADD16"|"QADDSUBX"|"QDADD"|"QDSUB"|"QSUB"|"QSUB16"|"QSUB8"|"QSUBADDX"|"SADD16"|"SADD8"|"SADDSUBX"|"SEL"|"SHADD16"|"SHADD8"|"SHADDSUBX"|"SHSUB16"|"SHSUB8"|"SHSUBADDX") :: _ 
+      | ("PKHBT"|"PKHTB"|"QADD"|"QADD8"|"QADD16"|"QADDSUBX"|"QDADD"|"QDSUB"|"QSUB"|"QSUB16"|"QSUB8"|"QSUBADDX"|"SADD16"|"SADD8"|"SADDSUBX"|"SEL"|"SHADD16"|"SHADD8"|"SHADDSUBX"|"SHSUB16"|"SHSUB8"|"SHSUBADDX"|"SSUB16"|"SSUB8"|"SSUBADDX") :: _ 
 	-> Or (NotPC ("n", 16), Or (NotPC ("d", 12), NotPC ("m", 0)))
-      | ("REV"|"REV16"|"REVSH") :: _ -> Or (NotPC ("d", 12), NotPC ("m", 0))
+      | ("REV"|"REV16"|"REVSH"|"SSAT"|"SSAT16"|"SXTAB"|"SXTAB16"|"SXTAH"|"SXTB"|"SXTB16"|"SXTH") :: _ -> Or (NotPC ("d", 12), NotPC ("m", 0))
       | "RFE" :: _ -> NotPC ("n", 16)
       | "SMLAD" :: _ -> Or (NotPC ("d",16), Or (NotPC ("m",0), NotPC ("s",8)))
       | "SMLAL" :: _ -> Or (NotPC ("dHi", 16), Or (NotPC ("dLo", 12), Or (NotPC ("s", 8), NotPC ("m",0))))
-      | ("SMLALxy"|"SMLALD") :: _ -> Or (NotPC ("dHi", 16), Or (NotPC ("dLo", 12), Or (NotPC ("s", 8), Or (NotPC ("m",0), NotSame ("dHi","dLo")))))
-
+      | ("SMLALxy"|"SMLALD"|"SMLSLD"|"SMULL"|"UMAAD"|"UMLAL"|"UMULL") :: _ -> Or (NotPC ("dHi", 16), Or (NotPC ("dLo", 12), Or (NotPC ("s", 8), Or (NotPC ("m",0), NotSame (16,12)))))
+      | ("SMMUL"|"SMUAD"|"SMULxy"|"SMULWy"|"SMUSD"|"USAD8"|"USADA8"):: _ -> Or (NotPC ("d", 16), Or (NotPC ("s", 8), NotPC ("m", 0)))
+      (*| STRD ->*)
+      | "STREX" :: _ -> Or (NotPC ("n",16), Or (NotPC ("d",12), Or (NotPC ("m",0), Or (NotSame (12,0), NotSame (12,16)))))
+      | "STRT" :: _ -> NotSame (16,12)
+      | ("SWP"|"SWPB") :: _ -> Or (NotPC ("n",16), Or (NotPC ("d",12), Or (NotPC ("m",0), Or (NotSame (16,0), NotSame (16,12)))))
+      | ("UADD16"|"UADD8"|"UADDSUBX"|"UHADD16"|"UHADD8"|"UHADDSUBX"|"UHSUB16"|"UHSUB8"|"UHSUBADDX"|"UQADD16"|"UQADD8"|"UQADDSUBX"|"UQSUB16"|"UQSUB8"|"UQSUBADDX"|"USUB16"|"USUB8"|"USUBADDX") :: _ -> Or (NotPC ("n",16), Or (NotPC ("d",12), NotPC ("m",0)))
+      | ("USAT"|"USAT16"|"UXTAB"|"UXTAB16"|"UXTAH"|"UXTB"|"UXTB16"|"UXTH") :: _ -> Or (NotPC ("d", 12), NotPC ("m",0))
       | _ -> General
   in
   let aux2 m w =
     match m with
-      | NotPC (_, i) -> insert_bits (Int32.of_int 0b1110) i w
+      | NotPC (_, i) -> insert_bits (Int32.of_int 0b1111) i w
+      | NotLR (_, i) -> insert_bits (Int32.of_int 0b1110) i w
       (*| NotOne i -> insert_bit Int32.one i w*)
       | NotZero i -> insert_bit (Int32.of_int 0b000000000000000) i w
       
       | NotV (_,i,p) -> insert_bit i p w
       | NotVs (_,s,p) ->  insert_bits (Int32.of_int s) p w
       | And (NotV (_, i1,p1), NotV (_,i2,p2)) -> insert_bit i1 p1 (insert_bits i2 p2 w)
-      | General | And _ | NotSame _ | IsEven _| OtherVC _ | NotLR _
-      | NotLSL0| NoWritebackDest|Or _
+      | NotSame (i1, i2) -> let r = (Random.int32 (Int32.of_int 0b1111)) in
+	  insert_bits r i1 (insert_bits r i2 w)
+      | NoWritebackDest -> insert_bit Int32.one 21 w
+      | General | And _ | IsEven _| OtherVC _
+      | NotLSL0|Or _
 	  -> push_bit (Random.bool ()) w
   in aux2 (aux lh ls) Int32.zero
 ;;
@@ -140,7 +148,8 @@ let gen_bin pc =
 let bin_inst out (lh, ls) =
   let md = add_mode (name (lh,ls)) in
     match md with
-      | DecInst -> output_word out (gen_bin ls)
+      | DecInst -> (*output_word out (gen_bin ls)*)
+	  output_word out (gen_unpred (lh, ls))
       | DecEncoding -> ()
       | DecMode _ -> ();;
 
