91bfdae1fb43082b882c7a8090b1124fcd7fd4e7
Claude Helmstetter <helmstet@users.gforge.inria.noreply>
2010-09-23 12:34:41 +0000

    inline the function ConditionPassed
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@662 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- simlight2/slv6_condition.c
+++ simlight2/slv6_condition.c
@@ -4,7 +4,6 @@
 /* The condition field */
 
 #include "slv6_condition.h"
-#include "slv6_status_register.h"
 
 BEGIN_SIMSOC_NAMESPACE
 
@@ -27,27 +26,6 @@ const char *condition2string(SLv6_Condition cond) {
   case SLV6_AL: return "AL";
   }
   abort();
-}
-
-bool ConditionPassed(struct SLv6_StatusRegister *sr, SLv6_Condition cond) {
-  switch (cond) {
-  case SLV6_EQ: return sr->Z_flag;
-  case SLV6_NE: return !sr->Z_flag;
-  case SLV6_CS_HS: return sr->C_flag;
-  case SLV6_CC_LO: return !sr->C_flag;
-  case SLV6_MI: return sr->N_flag;
-  case SLV6_PL: return !sr->N_flag;
-  case SLV6_VS: return sr->V_flag;
-  case SLV6_VC: return !sr->V_flag;
-  case SLV6_HI: return sr->C_flag && !sr->Z_flag;
-  case SLV6_LS: return !sr->C_flag || sr->Z_flag;
-  case SLV6_GE: return sr->N_flag == sr->V_flag;
-  case SLV6_LT: return sr->N_flag != sr->V_flag;
-  case SLV6_GT: return !sr->Z_flag && sr->N_flag == sr->V_flag;
-  case SLV6_LE: return sr->Z_flag || sr->N_flag != sr->V_flag;
-  case SLV6_AL: return true;
-  }
-  assert(false && "invalid cond"); abort();
 }
 
 END_SIMSOC_NAMESPACE
--- simlight2/slv6_condition.h
+++ simlight2/slv6_condition.h
@@ -7,6 +7,7 @@
 #define SLV6_CONDITION_H
 
 #include "common.h"
+#include "slv6_status_register.h"
 
 BEGIN_SIMSOC_NAMESPACE
 
@@ -18,7 +19,29 @@ typedef enum{SLV6_EQ, SLV6_NE, SLV6_CS_HS, SLV6_CC_LO,
 extern const char *condition2string(SLv6_Condition);
 
 struct SLv6_StatusRegister;
-extern bool ConditionPassed(struct SLv6_StatusRegister*, SLv6_Condition);
+
+/* extern bool ConditionPassed(struct SLv6_StatusRegister*, SLv6_Condition); */
+
+static inline bool ConditionPassed(struct SLv6_StatusRegister *sr, SLv6_Condition cond) {
+  switch (cond) {
+  case SLV6_EQ: return sr->Z_flag;
+  case SLV6_NE: return !sr->Z_flag;
+  case SLV6_CS_HS: return sr->C_flag;
+  case SLV6_CC_LO: return !sr->C_flag;
+  case SLV6_MI: return sr->N_flag;
+  case SLV6_PL: return !sr->N_flag;
+  case SLV6_VS: return sr->V_flag;
+  case SLV6_VC: return !sr->V_flag;
+  case SLV6_HI: return sr->C_flag && !sr->Z_flag;
+  case SLV6_LS: return !sr->C_flag || sr->Z_flag;
+  case SLV6_GE: return sr->N_flag == sr->V_flag;
+  case SLV6_LT: return sr->N_flag != sr->V_flag;
+  case SLV6_GT: return !sr->Z_flag && sr->N_flag == sr->V_flag;
+  case SLV6_LE: return sr->Z_flag || sr->N_flag != sr->V_flag;
+  case SLV6_AL: return true;
+  }
+  assert(false && "invalid cond"); abort();
+}
 
 END_SIMSOC_NAMESPACE
 
