4beb7c14fc2b8ccaa1104667a22f87d42a8fdd8e
Xiaomu Shi <shi@users.gforge.inria.noreply>
2010-09-28 09:18:21 +0000

    use flatten module
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@672 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- pseudocode/gendectest.ml
+++ pseudocode/gendectest.ml
@@ -18,6 +18,10 @@ open Codetype;; (* from the directory "refARMparsing" *)
 open Gencoqdec;;
 open Dec;;
 open Validity;;
+open Flatten;;
+
+let init = Random.init 1
+;;
 
 (* output a 32 bits word in little-endian *)
 let output_word out (word: int32) =
@@ -31,42 +35,12 @@ let push_bit (b: bool) (w: int32) =
   let w' = Int32.shift_left w 1 in
     if b then Int32.succ w' else w'
 
-(*let gen_unpre x =
-  let aux ls w =
-  match ls with
-    | Range (("m","n","s","d"),_,_) -> Int32.of_string "0o15"
-    | Shouldbe true -> push_bit Int32.zero w
-    | Shouldbe false -> push_bit Int32.one w
-    | Range _ -> push_bit (Random.bool ())
-    | Value _| Shouldbe _| Param1 _| Param1s _| Range _| Nothing
-;;
-*)
-
 let push_bits (x: bool array) (y: int32) =
   let lst = Array.to_list x in
     List.fold_right push_bit lst y;; 
 
 let push_pc (x: int32) = 
   push_bit false (push_bit true (push_bit true (push_bit true x)));;  
-
-type vconstraint =
-  | NotPC of string * int   (* the string must contains the name of parameter *)
-  | NotLR of string * int (* the string must contains the name of parameter *)
-  | IsEven of string   (* parameter that should contain an even value *)
-  (*| NotZero of int  (* parameter that should not be zero *)
-  | NotOne of int*)
-  | NoWritebackDest    (* no write-back with Rd==Rn *)
-  | NotSame of int * int (* R<a> <> R<b> *)
-  | NotLSL0            (* to distinguished between (equivalent?) mode cases *)
-  | OtherVC of exp     (* Other validy constraints described by a boolean
-                        * expression *)
-  | NotV of string * int32 * int
-  | NotVs of string * int * int
-  | And of vconstraint * vconstraint
-  | NotZero of int
-  | Or of vconstraint * vconstraint
-  | General
-;;
 
 let insert_bit (b: int32) (p: int) (w: int32) =
   let i = Int32.shift_left b p in
@@ -76,11 +50,78 @@ let insert_bits i p w =
   let is = Int32.shift_left i p in
     Int32.logor is w;;
 
-let gen_unpred (lh, ls) =
+type vcon =
+  | Insert_bits of int *int
+  | Insert_bit of bool * int
+  | No_change
+;;
+(*
+let gen_pre pc =
+  let aux ls w =
+    match ls with
+      | Value s -> push_bit s w
+      | Shouldbe s -> push_bit s w
+      | Range ("cond", _, _) -> push_bit (Random.bool ()) w
+      | Param1 _ | Param1s _ -> push_bit false w
+      | Range _ -> push_bit false w
+      | Nothing -> raise (Failure "unexpected case")
+  in Array.fold_right aux pc.fdec Int32.zero;;
+
+let gen_bin pc =
+  let aux ls w =
+    match ls with
+      | Value s -> push_bit s w
+      | Shouldbe s -> push_bit s w
+      | Param1 _ | Param1s _ -> push_bit (Random.bool ()) w
+      | Range _ -> push_bit (Random.bool ()) w
+      | Nothing -> raise (Failure "unexpected case")
+  in Array.fold_right aux pc Int32.zero;;
+*)
+
+let gen_tests ps =
+  let aux1 dec =
+    match dec with
+      | (Shouldbe b, p) -> Insert_bit (b,p)
+      | (Value i, p) -> Insert_bit (i, p)
+      | ((Range _ | Param1 _ | Param1s _ | Nothing), _) -> No_change
+  in
+  let max_v i1 i2 =
+    if i2 >= i1 then
+      int_of_float (2.0** (float (i2-i1+1)))
+    else 1
+  in
+  let aux2 para =
+    match para with
+      | (("n"|"m"|"s"|"d"|"dLo"|"dHi"),_, p2) -> Insert_bits (Random.int 15, p2)
+      | (("cond"),_, p2) -> Insert_bits (Random.int 15, p2)
+      | (_, p1, p2) -> 
+	  Insert_bits (Random.int (max_v p1 p2), p2)
+  in
+  let proc vs w =
+    match vs with
+      | Insert_bits (i, p) -> insert_bits (Int32.of_int i) p w
+      | Insert_bit (b, p) -> insert_bit (if b then Int32.one else Int32.zero) p w
+      | No_change -> w
+  in 
+  let pos dec =
+    let ar = Array.create (Array.length dec) (Nothing, 0) in
+      for i = 0 to Array.length dec - 1 do
+	ar.(i) <- (dec.(i), i)
+      done;
+    ar
+  in match ps.finstr with
+    | "ADC"  -> 
+	Int32.logor (Array.fold_right proc (Array.map aux1 (pos ps.fdec)) Int32.zero)
+	  (List.fold_right proc (List.map aux2 (ps.fparams)) Int32.zero)
+    | _ -> Int32.zero
+;;
+
+
+(*let gen_unpred (lh, ls) =
   let aux lh ls =
     match name (lh, ls) with
       | ("BLX2"|"BXJ") :: _ -> NotPC ("m", 0)
-      | "CLZ" :: _ -> And (NotPC ("m", 0), NotPC ("d", 12))
+      | "CLZ" :: _ -> Or (NotPC ("m", 0), NotPC ("d", 12))
       | "CPS"::_ -> Or (And (NotV ("mmod",Int32.zero,18), NotVs ("immod", 0b00,16)), 
 			Or (And (NotV ("mmod",Int32.zero,18), NotVs ("immod", 0b01,16)), 
 			    And (NotV ("mmod",Int32.one,18), NotVs ("immod", 0b01,16))))
@@ -113,46 +154,45 @@ let gen_unpred (lh, ls) =
       | "STRT" :: _ -> NotSame (16,12)
       | ("SWP"|"SWPB") :: _ -> Or (NotPC ("n",16), Or (NotPC ("d",12), Or (NotPC ("m",0), Or (NotSame (16,0), NotSame (16,12)))))
       | ("UADD16"|"UADD8"|"UADDSUBX"|"UHADD16"|"UHADD8"|"UHADDSUBX"|"UHSUB16"|"UHSUB8"|"UHSUBADDX"|"UQADD16"|"UQADD8"|"UQADDSUBX"|"UQSUB16"|"UQSUB8"|"UQSUBADDX"|"USUB16"|"USUB8"|"USUBADDX") :: _ -> Or (NotPC ("n",16), Or (NotPC ("d",12), NotPC ("m",0)))
-      | ("USAT"|"USAT16"|"UXTAB"|"UXTAB16"|"UXTAH"|"UXTB"|"UXTB16"|"UXTH") :: _ -> Or (NotPC ("d", 12), NotPC ("m",0))
+      | ("USAT"|"USAT16"|"UXTAB"|"UXTAB16"|"UXTAH"|"UXTB"|"UXTB16"|"UXT H") :: _ -> Or (NotPC ("d", 12), NotPC ("m",0))
       | _ -> General
   in
-  let aux2 m w =
+  let rec aux2 m w =
     match m with
-      | NotPC (_, i) -> insert_bits (Int32.of_int 0b1111) i w
-      | NotLR (_, i) -> insert_bits (Int32.of_int 0b1110) i w
-      (*| NotOne i -> insert_bit Int32.one i w*)
+      | NotPC (_, p) -> insert_bits (Int32.of_int 15) p w
+      | NotLR (_, p) -> insert_bits (Int32.of_int 14) p w
       | NotZero i -> insert_bit (Int32.of_int 0b000000000000000) i w
       
       | NotV (_,i,p) -> insert_bit i p w
       | NotVs (_,s,p) ->  insert_bits (Int32.of_int s) p w
-      | And (NotV (_, i1,p1), NotV (_,i2,p2)) -> insert_bit i1 p1 (insert_bits i2 p2 w)
+      (*| And (NotV (_, i1,p1), NotV (_,i2,p2)) -> insert_bit i1 p1 (insert_bits i2 p2 w)*)
       | NotSame (i1, i2) -> let r = (Random.int32 (Int32.of_int 0b1111)) in
 	  insert_bits r i1 (insert_bits r i2 w)
       | NoWritebackDest -> insert_bit Int32.one 21 w
-      | General | And _ | IsEven _| OtherVC _
-      | NotLSL0|Or _
-	  -> push_bit (Random.bool ()) w
-  in aux2 (aux lh ls) Int32.zero
+      | Or (v1, v2) -> if Random.bool () then (aux2 v1 w) else (aux2 v2 w)
+      | And (v1, v2) -> aux2 v1 (aux2 v2 w)
+      | General 
+	  -> gen_bin ls
+  in aux2 (aux lh ls) (gen_pre ls)
 ;;
 
-let gen_bin pc =
-  let aux ls w =
-    match ls with
-      | Value s -> push_bit s w
-      | Shouldbe s -> push_bit s w
-      | Param1 _ | Param1s _ -> push_bit (Random.bool ()) w
-      | Range _ -> push_bit (Random.bool ()) w
-      | Nothing -> raise (Failure "unexpected case")
-  in Array.fold_right aux pc Int32.zero;;
-
-let bin_inst out (lh, ls) =
-  let md = add_mode (name (lh,ls)) in
+let bin_inst out ps =
+  let md = add_mode (name ps) in
     match md with
       | DecInst -> (*output_word out (gen_bin ls)*)
-	  output_word out (gen_unpred (lh, ls))
       | DecEncoding -> ()
       | DecMode _ -> ();;
 
-let gen_test out ps =
+let gen_tests out _ dec =
   set_binary_mode_out out true;
-  List.iter (bin_inst out) ps;;
+  List.iter (bin_inst out) dec;;
+*)
+
+let bin_insts out fs =
+    output_word out (gen_tests fs)
+;;
+
+let gen_test out pcs decs =
+  set_binary_mode_out out true;
+  let fs: fprog list = flatten pcs decs in
+    List.iter (bin_insts out) fs;;
