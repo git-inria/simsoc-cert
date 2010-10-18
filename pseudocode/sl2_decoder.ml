393e7e6bb48f92ace59a1ef7a738fee9d30ebb79
Claude Helmstetter <helmstet@users.gforge.inria.noreply>
2010-10-18 15:33:14 +0000

    improve the decoder algortihm with a switch, such that it scales up
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@709 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- pseudocode/sl2_decoder.ml
+++ pseudocode/sl2_decoder.ml
@@ -17,6 +17,20 @@ open Flatten;;
 open Util;;
 open Printf;;
 
+(* Each decoder starts with a switch on the following opcode:
+ * - ARM: (cond<>15) followed by bits 27 to 20 (9 bits, 512 cases)
+ * - Thumb: bits 15 to 11 (5 bits, 32 cases)
+ *) 
+
+let binary k i =
+  let is_set n w =
+    Int32.logand Int32.one (Int32.shift_right_logical w n) <> Int32.zero in
+  let n = match k with ARM -> 9 | Thumb -> 5 in
+  let s = String.make n '0' in
+    for b = 0 to n-1 do
+      if is_set b (Int32.of_int i) then (s.[n-b-1] <- '1')
+    done; s;;
+
 (** Generation of the decoder *)
 
 module type DecoderConfig = sig
@@ -39,12 +53,32 @@ module DecoderGenerator (DC: DecoderConfig) = struct
   (*  * - bn: file basename *)
   (*  * - is: the instructions *)
   let decoder bn (k: fkind) (is: xprog list) =
+
+    let class_count = match k with ARM -> 512 | Thumb -> 32 in
+    let classes = Array.create class_count [] in
+    let classify p =
+      let mask, value = Gencxx.mask_value p.xprog.fdec in 
+      let opmask, opcode = match k with
+        | Thumb ->
+            let aux w = Int32.shift_right_logical w 11 in aux mask, aux value
+        | ARM ->
+            let aux w =
+              let w' = Int32.shift_right_logical (Int32.shift_left w 4) 24 in
+                if is_conditional p then w' else Int32.logor w' (Int32.of_int 0x100)
+            in aux mask, aux value
+      in for i = 0 to class_count -1 do
+          if Int32.logand (Int32.of_int i) opmask = opcode
+          then classes.(i) <- p :: classes.(i)
+          else ()
+        done
+    in List.iter classify is;
+
     (* Phase A: check bits fixed by the coding table *)
     let instA b p =
       let (mask, value) = Gencxx.mask_value p.xprog.fdec in
-      bprintf b "  if ((bincode&0x%08lx)==0x%08lx && %a) {\n"
+      bprintf b "    if ((bincode&0x%08lx)==0x%08lx && %a) {\n"
         mask value DC.instr_call p.xprog.fid;
-      bprintf b "    assert(!found); found = true;\n  }\n"
+      bprintf b "      assert(!found); found = true;\n    }\n"
   in
     (* Phase B: extract parameters and check validity *)
   let instB b p =
@@ -81,7 +115,22 @@ module DecoderGenerator (DC: DecoderConfig) = struct
     bprintf b "/* the main function, used by the ISS loop */\n";
     bprintf b "%a {\n" DC.main_prof k;
     bprintf b "  bool found = false;\n";
-    bprintf b "%a" (list "" instA) is;
+    ( match k with
+       | ARM ->
+           bprintf b "  const uint32_t c = bincode>>28;\n";
+           bprintf b
+             "  const uint32_t opcode = (c==15 ? 0x100 : 0) | (bincode<<4)>>24;\n"
+       | Thumb ->
+           bprintf b "  const uint16_t opcode = bincode>>11;\n"
+    );
+    bprintf b "  switch (opcode) {\n";
+    for i = 0 to class_count - 1 do
+      bprintf b "  case %d: /* %s */\n" i (binary k i);
+      if i+1=class_count || classes.(i) <> classes.(i+1) then
+        bprintf b "%a    break;\n" (list "" instA) classes.(i)
+      else ()
+    done;
+    bprintf b "  }\n";
     bprintf b "  %s\n}\n" DC.return_action;
     bprintf b "\nEND_SIMSOC_NAMESPACE\n";
     let s = if k = ARM then "arm" else "thumb" in
