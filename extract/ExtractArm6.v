0e535f931e4fb0494f825f1836161ed8b30ee59d
Xiaomu Shi <shi@users.gforge.inria.noreply>
2011-01-05 11:24:26 +0000

    prepare for using the decode functions in ocaml decoder
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@955 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- extract/ExtractArm6.v
+++ extract/ExtractArm6.v
@@ -20,21 +20,32 @@ Extract Inductive unit => "unit" [ "()" ].
 Extract Inductive bool => "bool" [ "true" "false" ].
 Extract Inductive option => "option" [ "Some" "None" ].
 Extract Inductive List.list => "list" [ "[]" "(::)" ].
- 
+
+(*Require arm6dec.
+Extract Constant arm6dec.decode_addr_mode1 => "Arm6mldec.decode_addr_mode1". 
+Extract Constant arm6dec.decode_addr_mode2 => "Arm6mldec.decode_addr_mode2". 
+Extract Constant arm6dec.decode_addr_mode3 => "Arm6mldec.decode_addr_mode3". 
+Extract Constant arm6dec.decode_addr_mode4 => "Arm6mldec.decode_addr_mode4". 
+Extract Constant arm6dec.decode_addr_mode5 => "Arm6mldec.decode_addr_mode5". 
+Extract Constant arm6dec.decode_unconditional => "Arm6mldec.decode_unconditional".
+Extract Constant arm6dec.decode_conditional => "Arm6mldec.decode_conditional".
+Extract Constant arm6dec.decode => "Arm6mldec.decode".
+*)
+
 Require Simul.
 Require Semantics.
 Require ZArith.
+Require Functions.
 
 Extraction NoInline Simul.decode_cond.
 Extraction NoInline Simul.DecUndefined.
 Extraction NoInline Simul.decode_cond_mode.
 Extraction NoInline Semantics.if_CurrentModeHasSPSR.
+(*Extraction NoInline Semantics.if_then.*)
+(*Extraction NoInline andb.*)
+
 (*Print Extraction Inline.*)
 
 
 Require Extraction arm6.
 Recursive Extraction Library arm6.
-
-
-
-
