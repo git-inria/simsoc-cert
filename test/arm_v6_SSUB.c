2d201fc206c99f65689fe260da14513669872648
Yang Yu <sony_tornado@users.gforge.inria.noreply>
2011-01-12 01:42:38 +0000

    split SS.c to SSAT.c & SSUB.c
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@986 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- test/arm_v6_SSUB.c
+++ test/arm_v6_SSUB.c
@@ -19,72 +19,6 @@ int GE3(uint32_t cpsr) {return (cpsr>>19)&1;}
 int GE10(uint32_t cpsr) {return (cpsr>>16)&3;}
 int GE32(uint32_t cpsr) {return (cpsr>>18)&3;}
 
-/* Signed Saturate saturates an optionally-shifted signed value to a selectable signed range. */
-/* The Q flag is set if the operation saturates. */
-void arm_SSAT_S(){
-  uint32_t x,q;
-  asm("msr cpsr_f, #0\n\t"
-      "ssat %0, #16, %2, ASR #28\n\t"
-      "mrs %1, CPSR\n\t"
-      : "=&r" (x), "=&r" (q)
-      : "r" (0x10000000));
-  CHECK((x == 0x1));
-  CHECK((Q_flag(q) == 0));
-}
-void arm_SSAT_SQ(){
-  uint32_t x,q;
-  asm("msr cpsr_f, #0\n\t"
-      "ssat %0, #16, %2, LSL #12\n\t"
-      "mrs %1, CPSR\n\t"
-      : "=&r" (x), "=&r" (q)
-      : "r" (0x1237fff8));
-  CHECK((x == 0x7fff));
-  CHECK((Q_flag(q) == 1));
-}
-void arm_SSAT(){
-  uint32_t x,q;
-  asm("msr cpsr_f, #0\n\t"
-      "ssat %0, #32, %2\n\t"
-      "mrs %1, CPSR\n\t"
-
-      : "=&r" (x), "=&r" (q)
-      : "r" (0x5678));
-  CHECK((x == 0x5678));
-  CHECK((Q_flag(q) == 0));
-}
-void arm_SSAT_Q(){
-  uint32_t x,q;
-  asm("msr cpsr_f, #0\n\t"
-      "ssat %0, #16, %2\n\t"
-      "mrs %1, CPSR\n\t"
-      : "=&r" (x), "=&r" (q)
-      : "r" (0x12345678));
-  CHECK((x == 0x7fff));
-  CHECK((Q_flag(q) == 1));
-}
-
-/* Signed Saturate 16 saturates two signed 16-bit values to a selected signed range. */
-/* The Q flag is set if the operation saturates. */
-void arm_SSAT16(){
-  uint32_t x,q;
-  asm("msr cpsr_f, #0\n\t"
-      "ssat16 %0, #8, %2\n\t"
-      "mrs %1, CPSR\n\t"
-      : "=&r" (x), "=&r" (q)
-      : "r" (0x10001));
-  CHECK((x == 0x10001));
-  CHECK((Q_flag(q) == 0));
-}
-void arm_SSAT16_Q(){
-  uint32_t x,q;
-  asm("msr cpsr_f, #0\n\t"
-      "ssat16 %0, #8, %2\n\t"
-      "mrs %1, CPSR\n\t"
-      : "=&r" (x), "=&r" (q)
-      : "r" (0x17001));
-  CHECK((x == 0x1007f));
-  CHECK((Q_flag(q) == 1));
-}
 
 /* Signed Subtract 16 perfor1s two 16-bit signed integer subtractions, and writes the results to the destination */
 /* register. It sets the APSR.GE bits according to the results of the subtractions. */
@@ -168,12 +102,6 @@ void arm_SSUBADDX_GE(){
 }
 
 int main() {
-  arm_SSAT_S();
-  arm_SSAT_SQ();
-  arm_SSAT();
-  arm_SSAT_Q();
-  arm_SSAT16();
-  arm_SSAT16_Q(); 
   arm_SSUB16();
   arm_SSUB16_GE();
   arm_SSUB8(); 
