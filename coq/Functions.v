2f225ce98be1cd8025c4fa15b62c7fdcaf3c0496
Xiaomu Shi <shi@users.gforge.inria.noreply>
2011-01-20 11:17:46 +0000

    fix bug in UnsignedSat
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@1025 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- coq/Functions.v
+++ coq/Functions.v
@@ -387,7 +387,7 @@ x     if 0 <= x < 2^n
 (****************************************************************************)
 
 Definition UnsignedSat (x : word) (n : nat) : word :=
-  if zlt x 0 then repr 0
+  if lt_0 x then repr 0
     else let k := two_power_nat n in
       if zlt x k then x
       else repr (k-1).
