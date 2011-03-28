2b323bcc38a0a407d57cec51f26567661309adcd
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2011-03-28 05:54:12 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@1131 ce709078-17db-4df8-89f4-d3e5bb8270d9

---
+++ arm6/parsing/preproc_pseudo.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+# SimSoC-Cert, a Coq library on processor architectures for embedded systems.
+# See the COPYRIGHTS and LICENSE files.
+
+# this script does the following modifications:
+
+# add semi-colons at the end of various expressions
+# permute comments and semi-colons
+# add missing "then" with some "if"
+# add "do" after "while" and "for"
+# replace dash by a space
+# replace "is" by "==", and "is not" by "!="
+# replace "== R15" by "== 15"
+# remove "value" of a register
+# replace some English expressions by a function call
+# fix some typos
+# inline some code
+# replace a strange dash character by a usual dash
+
+# WARNING: do not remove the last strange -e command !
+# it is due to an unusual dash character
+
+sed \
+    -e '/^ *[a-zA-Z][a-zA-Z0-9 _(\+)]* = /s|$|;|' \
+    -e '/^ *[a-zA-Z][a-zA-Z0-9 _(\+)]*\[[:0-9a-zA-Z_ ,+]*] *= /s|$|;|' \
+    -e '/^ *if [a-zA-Z][a-zA-Z0-9 _]* == [a-zA-Z0-9][a-zA-Z0-9 _]*  *then  *[a-zA-Z][]\[a-zA-Z0-9 _]* *= /s|$|;|' \
+    -e '/^ *MemoryAccess *(/s|$|;|' \
+    -e '/^ *MarkExclusiveLocal *(/s|$|;|' \
+    -e '/^ *MarkExclusiveGlobal *(/s|$|;|' \
+    -e '/^ *ClearExclusiveByAddress *(/s|$|;|' \
+    -e '/^ *ClearExclusiveLocal *(/s|$|;|' \
+    -e '/^ *send /s|$|;|' \
+    -e '/^ *load /s|$|;|' \
+    -e '/^ *Start /s|$|;|' \
+    -e '/^ *UNPREDICTABLE/s|$|;|' \
+    -e '/^ *Coprocessor/s|$|;|' \
+    -e '/^ *assert /s|$|;|' \
+    -e 's|\( *//.*\);$|;\1|' \
+    -e 's|\( */\*.*\*/\);|;\1|' \
+    -e 's|if (\(.*\))$|if (\1) then|' \
+    -e 's|^\( *\)if \(.*\)\([0-9)]\)$|\1if \2\3 then|' \
+    -e 's|^\( *\)while \(.*\)|\1while \2 do|' \
+    -e 's|^\( *\)for \(.*\);|\1for \2 do|' \
+    -e 's|-\([A-Za-z]\)| \1|g' \
+    -e 's|R\(.*\) is even numbered|is_even(\1)|' \
+    -e 's|R\(.*\) is R|\1 == |' \
+    -e 's|R\(.*\) is not R|\1 != |' \
+    -e 's|R\(.*\) == R|\1 == |' \
+    -e 's|value to|to|' \
+    -e 's|first value|first_value|' \
+    -e 's|second value|second_value|' \
+    -e 's|dependent operation|dependent_operation|g' \
+    -e 's|Mask|Mask()|g' \
+    -e 's|CP15\(.*\)bit|CP15\1bit()|' \
+    -e 's|v4 and earlier|v4_and_earlier()|' \
+    -e 's|v5 and above|v5_and_above()|' \
+    -e 's|architecture version 5 or above|v5_and_above()|' \
+    -e 's|ARMv5 or above|v5_and_above()|' \
+    -e 's|IMPLEMENTATION DEFINED CONDITION|IMPLEMENTATION_DEFINED_CONDITION()|g' \
+    -e 's|\(.*\) = SUB ARCHITECTURE DEFINED value|\1 = \1_SUB_ARCHITECTURE_DEFINED_value()|g' \
+    -e 's|high vectors configured|high_vectors_configured()|' \
+    -e 's|(JE bit of Main Configuration register)|JE_bit_of_Main_Configuration_register()|' \
+    -e 's|(CV bit of Jazelle OS Control register)|CV_bit_of_Jazelle_OS_Control_register()|' \
+    -e 's|(Jazelle Extension accepts opcode at jpc)|Jazelle_Extension_accepts_opcode_at(jpc)|' \
+    -e 's|address of BKPT instruction|address_of_current_instruction()|' \
+    -e 's|address of the instruction after the BLX instruction|address_of_next_instruction()|' \
+    -e 's|address of instruction after the BLX instruction|address_of_next_instruction()|' \
+    -e 's|address of next instruction after the SWI instruction|address_of_next_instruction()|' \
+    -e 's|address of the instruction after the branch instruction|address_of_next_instruction()|' \
+    -e 's|address of next instruction after the Undefined instruction|address_of_next_instruction()|' \
+    -e 's|address of the aborted instruction|address_of_current_instruction()|' \
+    -e 's|address of next instruction to be executed|address_of_next_instruction()|' \
+    -e 's|address of next instruction|address_of_next_instruction()|' \
+    -e 's|address of the instruction after this BLX|address_of_next_instruction()|' \
+    -e 's|CPSR = CPSR with specified E bit modification|CPSR[9] = E|' \
+    -e 's|VE==0|CP15_reg1_VEbit() == 0|' \
+    -e 's|(not overridden by debug hardware)|not_overridden_by_debug_hardware()|' \
+    -e "s|bit position of most significant'1' in Rm|bit_position_of_most_significant_1(Rm)|" \
+    -e 's|Start opcode execution at jpc|Start_opcode_execution_at(jpc)|' \
+    -e 's|8_bit_immediate|immed_8|' \
+    -e 's|8_bit_word_offset|offset_8|' \
+    -e 's|coprocessor\[|Coprocessor[|' \
+    -e 's|^\( *\)If |\1if |' \
+    -e 's|Artihmetic|Arithmetic|' \
+    -e 's|(diff4]|(diff4)|' \
+    -e 's|memory\[|Memory\[|' \
+    -e 's| pc | PC |' \
+    -e 's|<shift_imm>|shift_imm|' \
+    -e 's|See “Data processing operands - Rotate right with extend” on page A5-17|shifter_operand = (C Flag Logical_Shift_Left 31) OR (Rm Logical_Shift_Right 1); shifter_carry_out = Rm[0]|' \
+    -e 's|–|-|'
