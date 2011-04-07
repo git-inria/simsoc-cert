97b4cd08f91349685f480e23f7dccb4b1d8a3cf1
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2011-04-07 07:02:05 +0000

    added simsoc-cert header at the end
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@1293 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- simgen/extraction.v
+++ simgen/extraction.v
@@ -103,7 +103,23 @@ Extraction Blacklist List String Int.
 
 (* Go! *)
 Cd "extraction".
+
+(**
+SimSoC-Cert, a toolkit for generating certified processor simulators.
+
+See the COPYRIGHTS and LICENSE files.
+
+Formalization of the ARM architecture version 6 following the:
+
+ARM Architecture Reference Manual, Issue I, July 2005.
+
+Page numbers refer to ARMv6.pdf.
+
+Extraction of the arm6 simulator.
+*)
+
 Require Import RawCoq_Csyntax.
+
 Extraction Library NaryFunctions.
 Extraction Library Bvector.
 Extraction Library RawCoq_Csyntax.
