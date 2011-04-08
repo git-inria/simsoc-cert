f61322876673ef0ce90c0001095a57c07ee30ef0
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2011-04-08 09:47:03 +0000

    fixed required modules
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@1370 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- coq/extraction.v
+++ coq/extraction.v
@@ -6,7 +6,7 @@ See the COPYRIGHTS and LICENSE files.
 Extraction of the Arm6 simulator.
 *)
 
-Require Extraction Arm6_Simul.
+Require Extraction Simul.
 
 Extraction Library Bitvec.
 Extraction Library Util.
