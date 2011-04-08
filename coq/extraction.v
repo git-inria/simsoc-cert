0592b35be05db211d9393e7bac7f6c830b9103b2
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2011-04-08 09:44:32 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@1368 ce709078-17db-4df8-89f4-d3e5bb8270d9

---
+++ coq/extraction.v
@@ -0,0 +1,14 @@
+(**
+SimSoC-Cert, a toolkit for generating certified processor simulators.
+
+See the COPYRIGHTS and LICENSE files.
+
+Extraction of the Arm6 simulator.
+*)
+
+Require Extraction Arm6_Simul.
+
+Extraction Library Bitvec.
+Extraction Library Util.
+Extraction Library Semantics.
+Extraction Library Simul.
