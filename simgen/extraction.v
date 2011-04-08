7b819d27429504adfbc8aeb7c730ac2b55feeedf
Frédéric Blanqui <blanqui@users.gforge.inria.noreply>
2011-04-08 09:58:31 +0000

    nothing
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@1373 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- simgen/extraction.v
+++ simgen/extraction.v
@@ -3,12 +3,6 @@ SimSoC-Cert, a toolkit for generating certified processor simulators.
 
 See the COPYRIGHTS and LICENSE files.
 
-Formalization of the ARM architecture version 6 following the:
-
-ARM Architecture Reference Manual, Issue I, July 2005.
-
-Page numbers refer to ARMv6.pdf.
-
 Extraction of the arm6 simulator.
 *)
 
