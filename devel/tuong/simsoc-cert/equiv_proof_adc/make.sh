682156289e02f11a7d75aa33fffd185523f4ba63
Frédéric Tuong <tuong@users.gforge.inria.fr>
2011-06-20 12:44:21 +0000

    bugfix: the notion of relative directory
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@1607 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- devel/tuong/simsoc-cert/equiv_proof_adc/make.sh
+++ devel/tuong/simsoc-cert/equiv_proof_adc/make.sh
@@ -2,9 +2,9 @@
 set -x
 set -e
 
-ARCH=`sed -n -e 's/^ARCH=//p' ../../compcert/Makefile.config`
-VARIANT=`sed -n -e 's/^VARIANT=//p' ../../compcert/Makefile.config`
-P=../../
+P=../
+ARCH=`sed -n -e 's/^ARCH=//p' ${P}compcert/Makefile.config`
+VARIANT=`sed -n -e 's/^VARIANT=//p' ${P}compcert/Makefile.config`
 
 coqc \
   -I ${P}compcert/lib -I ${P}compcert/common -I ${P}compcert/$ARCH/$VARIANT -I ${P}compcert/$ARCH -I ${P}compcert/backend -I ${P}compcert/cfrontend \
