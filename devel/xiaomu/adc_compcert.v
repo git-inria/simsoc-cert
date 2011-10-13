4f7a4ff6c2b1af3d05f416a7bf1e43b25ac89236
Xiaomu Shi <shi@users.gforge.inria.noreply>
2011-10-13 08:26:46 +0000

    manually changed adc representation in Compcert-C
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@1791 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- devel/xiaomu/adc_compcert.v
+++ devel/xiaomu/adc_compcert.v
@@ -69,24 +69,34 @@ Definition OverflowFrom_add3 := 54.
 Definition CarryFrom_add3 := 55.
 Definition reg := 56.
 Definition get_bit := 57.
-Definition unpredictable := 58.
+Definition unpredicatable := 58.
 Definition spsr := 59.
 Definition copy_StatusRegister := 60.
 Definition CurrentModeHasSPSR := 61.
 Definition set_reg_or_pc := 62.
 Definition ConditionPassed := 63.
 Definition ADC := 64.
+
+(* manually add *)
+Definition x := 66.
+Definition addr_of_reg_m := 73.
+Definition reg_id := 75.
+Definition m := 76.
+Definition reg_m := 77.
+
 Close Scope positive_scope.
 
 (* structs and unions *)
 
-Definition typ_SLv6_SystemCoproc := Tstruct SLv6_SystemCoproc 
+Definition typ_struct_SLv6_SystemCoproc := 
 F[
   ee_bit -: uint8; 
   u_bit -: uint8; 
   v_bit -: uint8].
 
-Definition typ_SLv6_StatusRegister := Tstruct SLv6_StatusRegister 
+Definition typ_SLv6_SystemCoproc := Tstruct SLv6_SystemCoproc typ_struct_SLv6_SystemCoproc.
+
+Definition typ_struct_SLv6_StatusRegister := 
 F[
   N_flag -: uint8; 
   Z_flag -: uint8; 
@@ -106,14 +116,18 @@ F[
   mode -: uint32; 
   background -: uint32].
 
-Definition typ_SLv6_MMU := Tstruct SLv6_MMU 
+Definition typ_SLv6_StatusRegister := Tstruct SLv6_StatusRegister typ_struct_SLv6_StatusRegister.
+
+Definition typ_struct_SLv6_MMU := 
 F[
   begin -: uint32; 
   size -: uint32; 
   _end -: uint32; 
   mem -: `*` uint8].
 
-Definition typ_SLv6_Processor := Tstruct SLv6_Processor 
+Definition typ_SLv6_MMU := Tstruct SLv6_MMU typ_struct_SLv6_MMU.
+
+Definition typ_struct_SLv6_Processor := 
 F[
   mmu_ptr -: `*` typ_SLv6_MMU; 
   cpsr -: typ_SLv6_StatusRegister; 
@@ -129,20 +143,33 @@ F[
   pc -: `*` uint32; 
   jump -: uint8].
 
+Definition typ_SLv6_Processor := Tstruct SLv6_Processor typ_struct_SLv6_Processor.
+
 (* expression types *)
 
 Definition T1 := uint32.
 Definition T2 := Tfunction T[`*` typ_SLv6_Processor; uint8] uint32.
-Definition T3 := typ_SLv6_Processor.
+Definition T3 := `*` typ_SLv6_Processor.
 Definition T4 := uint8.
-Definition T5 := Tfunction T[`*` typ_SLv6_Processor; uint32] uint8.
-Definition T6 := typ_SLv6_StatusRegister.
-Definition T7 := int32.
-Definition T8 := Tfunction T[`*` typ_SLv6_Processor; uint8; uint32] void.
-Definition T9 := void.
-Definition T10 := Tfunction T[`*` typ_SLv6_Processor] uint8.
-Definition T11 := Tfunction T[`*` typ_SLv6_StatusRegister; uint32] void.
-Definition T12 := Tfunction T[uint32; uint32] uint8.
+Definition T5 := Tfunction T[`*` typ_SLv6_StatusRegister; uint32] uint8.
+Definition T6 := typ_SLv6_Processor.
+Definition T7 := typ_SLv6_StatusRegister.
+Definition T8 := `*` typ_SLv6_StatusRegister.
+Definition T9 := int32.
+Definition T10 := Tfunction T[`*` typ_SLv6_Processor; uint8; uint32] void.
+Definition T11 := void.
+Definition T12 := Tfunction T[`*` typ_SLv6_Processor] uint8.
+Definition T13 := Tfunction T[`*` typ_SLv6_StatusRegister; `*` typ_SLv6_StatusRegister] void.
+Definition T14 := Tfunction T[`*` typ_SLv6_StatusRegister] (`*` typ_SLv6_StatusRegister).
+Definition T15 := Tfunction T[] void.
+Definition T16 := Tfunction T[uint32; uint32] uint8.
+Definition T17 := Tfunction T[uint32; uint32; uint32] uint8.
+Definition T18 := Tfunction T[uint32; uint32; uint8] uint8.
+
+(* manually add *)
+Definition T19 := Tfunction T[`*` typ_SLv6_Processor; uint8; int32] uint32.
+Definition T20 := Tfunction T[`*` typ_SLv6_Processor; uint8; int32] (`*` uint32).
+Definition T21 := `*` uint32.
 
 (* global variables *)
 
@@ -152,13 +179,79 @@ Definition gv_gvars :=
      gvar_readonly := false;
      gvar_volatile := false |}.
 
-Definition global_variables := [(gvars,gv_gvars)].
+Definition global_variables : list (prod ident (globvar type)) := [(gvars,gv_gvars)].
 
 (* functions *)
 
-Definition fun_ADC :=
-  (ADC, Internal
-{| fn_return := void;
+(* manually add ***********************************************************************************)
+Definition fun_internal_get_bit :=
+  {| fn_return := uint8;
+     fn_params := [
+x -: uint32; 
+n -: uint32];
+     fn_vars := [];
+     fn_body :=
+return (Some (((\x`:T1)>>(\n`:T1)`:T1)&(#1`:T9)`:T1)) |}.
+Definition fun_get_bit :=
+  (get_bit, Internal fun_internal_get_bit).
+
+Definition fun_addr_of_reg_m :=
+  (addr_of_reg_m, External
+  {| ef_id := addr_of_reg_m;
+     ef_sig := {| sig_args := [AST.Tint; AST.Tint; AST.Tint]; sig_res := Some AST.Tint |};
+     ef_inline := false |}
+  T[`*` typ_SLv6_Processor; uint8; int32]
+  (`*` uint32)).
+
+Definition fun_internal_reg_m :=
+  {| fn_return := uint32;
+     fn_params := [
+proc -: `*` typ_SLv6_Processor; 
+reg_id -: uint8; 
+m -: int32];
+     fn_vars := [];
+     fn_body :=
+return (Some (valof (`*(call (\addr_of_reg_m`:T20) E[\proc`:T3; \reg_id`:T4; \m`:T9] T21)`:T1) T1)) |}.
+
+Definition fun_reg_m :=
+  (reg_m, Internal fun_internal_reg_m).
+
+Definition fun_internal_reg :=
+  {| fn_return := uint32;
+     fn_params := [
+proc -: `*` typ_SLv6_Processor; 
+reg_id -: uint8];
+     fn_vars := [];
+     fn_body :=
+return (Some (call (\reg_m`:T19) E[\proc`:T3; \reg_id`:T4; (valof (((`*(\proc`:T3)`:T6)|cpsr`:T7)|mode`:T9) T9)] T1)) |}.
+
+Definition fun_reg :=
+  (reg, Internal fun_internal_reg).
+
+Definition fun_ConditionPassed :=
+  (ConditionPassed, External
+  {| ef_id := ConditionPassed;
+     ef_sig := {| sig_args := [AST.Tint; AST.Tint]; sig_res := Some AST.Tint |};
+     ef_inline := false |}
+  T[`*` typ_SLv6_StatusRegister; int32]
+  uint8).
+
+Definition fun_internal_CurrentModeHasSPSR :=
+  {| fn_return := uint8;
+     fn_params := [
+proc -: `*` typ_SLv6_Processor];
+     fn_vars := [];
+     fn_body :=
+return (Some ((valof (((`*(\proc`:T3)`:T6)|cpsr`:T7)|mode`:T9) T9)<(#5`:T9)`:T9)) |}.
+
+Definition fun_CurrentModeHasSPSR :=
+  (CurrentModeHasSPSR, Internal fun_internal_CurrentModeHasSPSR).
+
+(**************************************************************************************************)
+
+(* generated *)
+Definition fun_internal_ADC :=
+  {| fn_return := void;
      fn_params := [
 proc -: `*` typ_SLv6_Processor; 
 S -: uint8; 
@@ -170,21 +263,26 @@ shifter_operand -: uint32];
 old_Rn -: uint32];
      fn_body :=
 ($ old_Rn`:T1) `= (call (\reg`:T2) E[\proc`:T3; \n`:T4] T1)`:T1;;
-`if (call (\ConditionPassed`:T5) E[&((`*(\proc`:T3)`:T6)|cpsr`:T6)`:T6; \cond`:T7] T5)
-then (call (\set_reg_or_pc`:T8) E[\proc`:T3; \d`:T4; ((\old_Rn`:T1)+(\shifter_operand`:T1)`:T1)+(valof (((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|C_flag`:T4) T4)`:T1] T9);;
-`if (((\S`:T4)==(#1`:T7)`:T7)?(((\d`:T4)==(#15`:T7)`:T7)?(#1`:T7)`:(#0`:T7)`:T7)`:(#0`:T7)`:T7)
-then `if (call (\CurrentModeHasSPSR`:T10) E[\proc`:T3] T10)
-then (call (\copy_StatusRegister`:T11) E[&((`*(\proc`:T3)`:T6)|cpsr`:T6)`:T6; (call (\spsr`:T6) E[\proc`:T3] T6)] T9)
-else (call (\unpredictable`:T9) E[] T9)
-else `if ((\S`:T4)==(#1`:T7)`:T7)
-then (((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|N_flag`:T4) `= (call (\get_bit`:T12) E[(call (\reg`:T2) E[\proc`:T3; \d`:T4] T1); #31`:T7] T4)`:T4;;
-(((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|Z_flag`:T4) `= (((call (\reg`:T2) E[\proc`:T3; \d`:T4] T1)==(#0`:T7)`:T7)?(#1`:T7)`:(#0`:T7)`:T7)`:T4;;
-(((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|C_flag`:T4) `= (call (\CarryFrom_add3`:T9) E[\old_Rn`:T1; \shifter_operand`:T1; (valof (((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|C_flag`:T4) T4)] T4)`:T4;;
-(((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|V_flag`:T4) `= (call (\OverflowFrom_add3`:T9) E[(valof (\old_Rn`:T1) T1); \shifter_operand`:T1; (valof (((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|C_flag`:T4) T4)] T4)`:T4
+`if (call (\ConditionPassed`:T5) E[&((`*(\proc`:T3)`:T6)|cpsr`:T7)`:T8; \cond`:T9] T4)
+(*fix the trans *)
+then (call (\set_reg_or_pc`:T10) E[\proc`:T3; \d`:T4; ((\old_Rn`:T1)+(\shifter_operand`:T1)`:T1)+
+(valof (((`*(\proc`:T3)`:T6)|cpsr`:T7)|C_flag`:T4) T9)`:T9] T11);;
+`if (((\S`:T4)==(#1`:T9)`:T9)?(((\d`:T4)==(#15`:T9)`:T9)?(#1`:T9)`:(#0`:T9)`:T9)`:(#0`:T9)`:T9)
+then `if (call (\CurrentModeHasSPSR`:T12) E[\proc`:T3] T4)
+then (call (\copy_StatusRegister`:T13) E[&((`*(\proc`:T3)`:T6)|cpsr`:T7)`:T8; (call (\spsr`:T14) E[\proc`:T3] T8)] T11)
+else (call (\unpredicatable`:T15) E[] T11)
+else `if ((\S`:T4)==(#1`:T9)`:T9)
+then (((`*(\proc`:T3)`:T6)|cpsr`:T7)|N_flag`:T4) `= (call (\get_bit`:T16) E[(call (\reg`:T2) E[\proc`:T3; \d`:T4] T1); #31`:T9] T4)`:T4;;
+(((`*(\proc`:T3)`:T6)|cpsr`:T7)|Z_flag`:T4) `= (((call (\reg`:T2) E[\proc`:T3; \d`:T4] T1)==(#0`:T9)`:T9)?(#1`:T9)`:(#0`:T9)`:T9)`:T4;;
+(((`*(\proc`:T3)`:T6)|cpsr`:T7)|C_flag`:T4) `= (call (\CarryFrom_add3`:T17) E[\old_Rn`:T1; \shifter_operand`:T1; (valof (((`*(\proc`:T3)`:T6)|cpsr`:T7)|C_flag`:T4) T4)] T4)`:T4;;
+(((`*(\proc`:T3)`:T6)|cpsr`:T7)|V_flag`:T4) `= (call (\OverflowFrom_add3`:T18) E[\old_Rn`:T1; \shifter_operand`:T1; (valof (((`*(\proc`:T3)`:T6)|cpsr`:T7)|C_flag`:T4) T4)] T4)`:T4
 else skip
-else skip |}).
+else skip |}.
 
-Definition functions := [fun_ADC].
+Definition fun_ADC :=
+  (ADC, Internal fun_internal_ADC).
+
+Definition functions : list (prod ident fundef) := [fun_ADC].
 
 (* program *)
 
@@ -193,79 +291,13 @@ Definition p :=
      prog_main := main;
      prog_vars := global_variables |}.
 
+(* manually changed *)
+Definition prog_adc_funs := 
+  cons fun_addr_of_reg_m (cons fun_reg_m (cons fun_reg (cons fun_get_bit 
+    (cons fun_ConditionPassed (cons fun_CurrentModeHasSPSR functions))))).
 
-Definition fld_mmu :fieldlist:=
-  F[
-    begin -: uint32; 
-    size -: uint32; 
-    _end -: uint32; 
-    mem -: `*` uint8].
-
-Definition fld_sc :fieldlist:=
-  F[
-    ee_bit -: uint8; 
-    u_bit -: uint8; 
-    v_bit -: uint8].
-
-Definition fld_sr :fieldlist:=
-F[
-  N_flag -: uint8; 
-  Z_flag -: uint8; 
-  C_flag -: uint8; 
-  V_flag -: uint8; 
-  Q_flag -: uint8; 
-  J_flag -: uint8; 
-  GE0 -: uint8; 
-  GE1 -: uint8; 
-  GE2 -: uint8; 
-  GE3 -: uint8; 
-  E_flag -: uint8; 
-  A_flag -: uint8; 
-  I_flag -: uint8; 
-  F_flag -: uint8; 
-  T_flag -: uint8; 
-  mode -: uint32; 
-  background -: uint32].
-
-Definition fld_proc :fieldlist:=
-F[
-  mmu_ptr -: `*` typ_SLv6_MMU; 
-  cpsr -: typ_SLv6_StatusRegister; 
-  spsrs -: Tarray typ_SLv6_StatusRegister 5; 
-  cp15 -: typ_SLv6_SystemCoproc; 
-  id -: int8; 
-  user_regs -: Tarray uint32 16; 
-  fiq_regs -: Tarray uint32 7; 
-  irq_regs -: Tarray uint32 2; 
-  svc_regs -: Tarray uint32 2; 
-  abt_regs -: Tarray uint32 2; 
-  und_regs -: Tarray uint32 2; 
-  pc -: `*` uint32; 
-  jump -: uint8].
-
-Definition ADC_body :function:=
-{| fn_return := void;
-     fn_params := [
-proc -: `*` typ_SLv6_Processor; 
-S -: uint8; 
-cond -: int32; 
-d -: uint8; 
-n -: uint8; 
-shifter_operand -: uint32];
-     fn_vars := [
-old_Rn -: uint32];
-     fn_body :=
-($ old_Rn`:T1) `= (call (\reg`:T2) E[\proc`:T3; \n`:T4] T1)`:T1;;
-`if (call (\ConditionPassed`:T5) E[&((`*(\proc`:T3)`:T6)|cpsr`:T6)`:T6; \cond`:T7] T5)
-then (call (\set_reg_or_pc`:T8) E[\proc`:T3; \d`:T4; ((\old_Rn`:T1)+(\shifter_operand`:T1)`:T1)+(valof (((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|C_flag`:T4) T4)`:T1] T9);;
-`if (((\S`:T4)==(#1`:T7)`:T7)?(((\d`:T4)==(#15`:T7)`:T7)?(#1`:T7)`:(#0`:T7)`:T7)`:(#0`:T7)`:T7)
-then `if (call (\CurrentModeHasSPSR`:T10) E[\proc`:T3] T10)
-then (call (\copy_StatusRegister`:T11) E[&((`*(\proc`:T3)`:T6)|cpsr`:T6)`:T6; (call (\spsr`:T6) E[\proc`:T3] T6)] T9)
-else (call (\unpredictable`:T9) E[] T9)
-else `if ((\S`:T4)==(#1`:T7)`:T7)
-then (((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|N_flag`:T4) `= (call (\get_bit`:T12) E[(call (\reg`:T2) E[\proc`:T3; \d`:T4] T1); #31`:T7] T4)`:T4;;
-(((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|Z_flag`:T4) `= (((call (\reg`:T2) E[\proc`:T3; \d`:T4] T1)==(#0`:T7)`:T7)?(#1`:T7)`:(#0`:T7)`:T7)`:T4;;
-(((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|C_flag`:T4) `= (call (\CarryFrom_add3`:T9) E[\old_Rn`:T1; \shifter_operand`:T1; (valof (((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|C_flag`:T4) T4)] T4)`:T4;;
-(((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|V_flag`:T4) `= (call (\OverflowFrom_add3`:T9) E[(valof (\old_Rn`:T1) T1); \shifter_operand`:T1; (valof (((`*(\proc`:T3)`:(`*` T3))|cpsr`:T6)|C_flag`:T4) T4)] T4)`:T4
-else skip
-else skip |}.
+Definition prog_adc :=
+  {|
+    AST.prog_funct := prog_adc_funs;
+    AST.prog_main := main;
+    AST.prog_vars := global_variables |}.
