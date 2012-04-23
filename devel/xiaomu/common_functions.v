d670f9825bea4d7412e6221b4ee5aa463f87c7b6
Xiaomu Shi <shi@users.gforge.inria.noreply>
2012-04-23 09:40:26 +0000

    more lemmas on common functions
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/simsoc-cert@1870 ce709078-17db-4df8-89f4-d3e5bb8270d9

--- devel/xiaomu/common_functions.v
+++ devel/xiaomu/common_functions.v
@@ -1,3 +1,13 @@
+(**
+SimSoC-Cert, a toolkit for generating certified processor simulators.
+
+See the COPYRIGHTS and LICENSE files.
+
+For those library functions, we have general lemma for them in this file.
+Then these lemmas can be shared in the operation correctness proofs. 
+*)
+
+
 Require Import Globalenvs Memory.
 Require Import Csyntax Csem Cstrategy Coqlib Integers Values Maps Errors. 
 Require Import Arm6_State Arm6_Proc Arm6_SCC Bitvec Arm6.
@@ -10,7 +20,15 @@ Import Arm6_Functions.Semantics.
 Require Import my_inversion.
 Require Import my_tactic.
 
-Section ConditionPass_fun.
+(* The CompcertC generator outputs compcert-c file for each instruction.
+   Every file will have the declarations for identifiers and types.
+   Different file will give different value to the same identifier.
+   The proofs on same function calls can't be reused.
+   It is better to put these common used function and their proofs in 
+   the same place, and give abstruct values to identifiers.
+*)
+
+Section Common_fun.
 
 Variable N_flag : positive.
 Variable Z_flag : positive.
@@ -59,12 +77,6 @@ Variable SLv6_StatusRegister : positive.
 
 Definition typ_SLv6_StatusRegister :=
   Tstruct SLv6_StatusRegister typ_struct_SLv6_StatusRegister.
-
-(*
-Definition T1 :=
-Tfunction
-  (Tcons (Tpointer typ_SLv6_StatusRegister) (Tcons (Tint I32 Signed) Tnil))
-  (Tint I8 Signed).
 
 Variable begin : positive.
 Variable size : positive.
@@ -135,6 +147,12 @@ Variable SLv6_Processor : positive.
 Definition typ_SLv6_Processor := 
   Tstruct SLv6_Processor typ_struct_SLv6_Processor.
 
+Definition T1 :=
+  Tfunction
+  (Tcons (Tpointer typ_SLv6_StatusRegister) 
+    (Tcons (Tint I32 Signed) Tnil))
+  (Tint I8 Signed).
+
 Definition T2 := Tpointer typ_SLv6_Processor.
 
 Definition T3 := typ_SLv6_Processor.
@@ -146,11 +164,29 @@ Definition T5 := Tpointer typ_SLv6_StatusRegister.
 Definition T6 := Tint I32 Signed.
 
 Definition T7 := Tint I8 Signed.
+  
+Definition T8 := Tint I32 Unsigned.
+
+Definition T9 := Tint I8 Unsigned.
+
+Definition T10 :=
+  Tfunction
+  (Tcons (Tpointer typ_SLv6_Processor)
+    (Tcons (Tint I8 Unsigned) Tnil)) (Tint I32 Unsigned).
+
+Definition T11 :=
+  Tfunction
+  (Tcons (Tpointer typ_SLv6_Processor)
+    (Tcons (Tint I8 Unsigned) (Tcons (Tint I32 Unsigned) Tnil))) Tvoid.
+
+Definition T12 := Tvoid.
 
 Variable proc : positive.
-Variable cond :positive.*)
-
+Variable cond :positive.
 Variable ConditionPassed : positive.
+Variable shifter_operand :positive.
+
+(* ConditionPassed *)
 
 (*
 Definition condpass :=
@@ -189,5 +225,224 @@ Proof.
   reflexivity.
 Qed.
 
-End ConditionPass_fun.
+(* reg *)
 
+Variable reg : positive.
+
+Definition reg_r id :=
+  Ecall (Evalof (Evar reg T10) T10)
+  (Econs (Evalof (Evar proc T2) T2)
+    (Econs 
+      (Evalof (Evar id T9) T9) Enil)) T8.
+
+Lemma same_result_reg_content :
+  forall m e l b st rg r ge v,
+    proc_state_related m e (Ok tt (mk_semstate l b st)) ->
+    reg_proj m e rg = r ->
+    eval_simple_rvalue ge e m (reg_r rg) (Vint v) ->
+    v = Arm6_State.reg_content st r.
+Proof.  
+Admitted.
+
+Lemma proc_state_not_changed_reg_content :
+  forall m e l b st ge rg t m' a',
+    proc_state_related m e (Ok tt (mk_semstate l b st)) ->
+    eval_expr ge e m RV (reg_r rg) t m' a' ->
+    param_val proc m e = param_val proc m' e.
+Proof.
+Admitted.    
+
+(* set_reg_or_pc *)
+
+Variable old_Rn :positive.
+Variable d :positive.
+Variable set_reg_or_pc : positive.
+
+(*
+Definition add_old_Rn_so_Cf := 
+  (Ebinop Oadd
+    (Ebinop Oadd
+      (Evalof (Evar old_Rn T8) T8)
+      (Evalof (Evar shifter_operand T8) T8)
+      T8)
+    (Evalof
+      (Efield
+        (Efield
+          (Ederef
+            (Evalof (Evar proc T2) T2)
+            T6) cpsr T9) C_flag T7) T7)
+    T7).
+*)
+
+Definition set_regpc src :=
+  Ecall (Evalof (Evar set_reg_or_pc T11) T11)
+  (Econs (Evalof (Evar proc T2) T2)
+    (Econs (Evalof (Evar d T9) T9)
+      (Econs src Enil))) T12.
+
+Lemma same_set_reg_or_pc :
+  forall e m l b s rd ge src t m' a' v,
+    proc_state_related m e (Ok tt (mk_semstate l b s)) ->
+    d_func_related m e rd ->
+    eval_expr ge e m RV (set_regpc src) t m' a' ->
+    eval_simple_rvalue ge e m (set_regpc src) (Vint v) ->
+    (forall l b, proc_state_related m' e 
+      (Ok tt (mk_semstate l b
+        (Arm6_State.set_reg s rd v)))).
+Proof.
+Admitted.
+
+(* set_reg *)
+
+Variable set_reg : positive.
+
+Definition set_reg_number nm src :=
+  Ecall (Evalof (Evar set_reg T11) T11)
+  (Econs (Evalof (Evar proc T2) T2)
+    (Econs (Eval (Vint (repr nm)) T6)
+      (Econs src Enil))) T12.
+
+Lemma same_set_reg_number :
+  forall m e l b s ge rg src t m' a' v,
+    proc_state_related m e (Ok tt (mk_semstate l b s)) ->
+    eval_expr ge e m RV (set_reg_number rg src) t m' a' ->
+    eval_simple_rvalue ge e m (set_reg_number rg src) (Vint v) ->
+    (forall l b,proc_state_related m' e
+      (Ok tt (mk_semstate l b 
+        (Arm6_State.set_reg s (mk_regnum rg) v)))).
+Proof.
+Admitted.
+
+Definition set_reg_ref r src :=
+  Ecall (Evalof (Evar set_reg T11) T11)
+  (Econs (Evalof (Evar proc T2) T2)
+    (Econs (Evalof (Evar r T9) T9)
+      (Econs src Enil))) T12.
+
+Lemma same_set_reg_ref :
+  forall m e l b s ge rg r src t m' a' v,
+    proc_state_related m e (Ok tt (mk_semstate l b s)) ->
+    reg_proj m e rg = r ->
+    eval_expr ge e m RV (set_reg_ref rg src) t m' a' ->
+    eval_simple_rvalue ge e m (set_reg_ref rg src) (Vint v) ->
+    (forall l b,proc_state_related m' e
+      (Ok tt (mk_semstate l b 
+        (Arm6_State.set_reg s (mk_regnum r) v)))).
+Proof.
+Admitted.
+
+(* set_pc_raw *)
+
+Variable set_pc_raw :positive.
+
+Definition T13 :=
+  Tfunction
+  (Tcons (Tpointer typ_SLv6_Processor)
+    (Tcons (Tint I32 Unsigned) Tnil)) Tvoid.
+  
+
+Definition set_pc_raw_src src :=
+  Ecall (Evalof (Evar set_pc_raw T13) T13)
+  (Econs (Evalof (Evar proc T2) T2)
+    (Econs src Enil)) T12.
+
+Lemma same_set_pc_raw :
+  forall m e l b s ge t m' a' src v,
+    proc_state_related m e (Ok tt (mk_semstate l b s)) ->
+    eval_expr ge e m RV (set_pc_raw_src src) t m' a' ->
+    eval_simple_rvalue ge e m (set_pc_raw_src src) (Vint v) ->
+    (forall l b,proc_state_related m' e
+      (Ok tt (mk_semstate l b 
+        (Arm6_State.set_reg s PC v)))).
+Proof.
+Admitted.
+
+
+End Common_fun.
+
+(* Calling to a external function *)
+
+(* The semantics of system call tells us, that any call to external function 
+   will not change the current memory state *)
+
+Lemma mem_not_changed_ef :
+  forall ge m nm sg tplst tp vargs t m' v,
+    eval_funcall ge m (External (AST.EF_external nm sg) tplst tp) vargs t m' v ->
+    m = m'.
+Proof.
+  intros until v. intro evfun.
+  inv evfun. simpl in H7.
+  destruct H7.
+  reflexivity.
+Qed.
+
+
+(* Calling to an internal function, from memory state m to m'. 
+   alloc_variables m to m1;
+   bind_parameters m1 to m2;
+   exect_stmt m2 to m3;
+   free_list m3 to m'.
+   If there is no change from m2 to m3, the result to free_list gives the
+   same result (m = m') *)
+
+(*
+Lemma free_list_to_initialst :
+  forall ps_n vs_n ps vs m e m1 vargs m2 bd t out m' rt v,
+    list_norepet (ps_n ++ vs_n) ->
+    alloc_variables empty_env m (ps ++ vs) e m1 ->
+    bind_parameters e m1 ps vargs m2 ->
+    exec_stmt ge e m2 bd t m2 out ->
+    outcome_result_value out rt v ->
+    Mem.free_list m2 (blocks_of_env e) = Some m'.
+*)
+
+(*
+Lemma free_list_all_some :
+  forall m lst m',
+    Mem.free_list m lst = Some m' ->
+    (forall b lo hi, In (b, lo, hi) lst ->
+      exists mi mj, Mem.free mi b lo hi = Some mj).
+*)
+
+(*
+Lemma free_list_to_initial :
+  forall b lo hi ofs e lst m m' ty,
+    ~(In (b,lo,hi) lst) ->
+    (blocks_of_env e) = lst ->
+    Mem.free_list m lst = Some m'->
+    load_value_of_type ty m b ofs =
+    load_value_of_type ty m' b ofs.
+Proof.
+  intros until ty. intros not_in boe fl.
+  destruct lst.
+   simpl in fl.
+   injection fl;intros;subst;clear fl.
+   reflexivity.
+
+   unfold load_value_of_type;simpl.
+   destruct (access_mode ty);try reflexivity.
+   symmetry.
+
+   case_eq (Mem.free_list m (p :: lst));
+   [intros m1 fls|intros n;rewrite n in fl;clear n;discriminate].  
+
+   apply (Mem.load_free m b lo hi m').
+   admit. left. 
+*)
+    
+
+(* Equivalence of arithmetic definitions *)
+Lemma eq_getbit :
+  forall x n ,
+    0 < n < Z_of_nat wordsize ->
+    sign_ext 8 (and (shru x (repr n)) (repr 1)) = x [nat_of_Z n].
+Admitted.
+
+(* Finding a function in globalenv *)
+(*
+Lemma find_f :
+  forall ge fid b t m vf,
+    Genv.find_symbol ge fid = Some b ->
+    load_value_of_type t m b w0 = Some vf ->
+    Genv.find_funct ge vf = Some fd
+*)
